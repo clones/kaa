d3d19ddf788b52631e39d65166a853a5265d943c
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2003-06-08 15:46:31 +0000

    make it possible to use the cache
    git-svn-id: svn://svn.freevo.org/kaa/trunk@58 a8f5125c-1e01-0410-8897-facf34644b8e

--- mmpython/mediatest.py
+++ mmpython/mediatest.py
@@ -5,7 +5,13 @@ sys.path.append('..')
 
 import mmpython
 
+USE_CACHE = 1
+
+if USE_CACHE:
+    mmpython.use_cache('/tmp/')
+
 medium = mmpython.parse(sys.argv[1])
+
 #medium.expand_keywords()
 if medium:
     print "medium is: %s" % medium.type
@@ -13,3 +19,7 @@ if medium:
 else:
     print "No Match found"
 
+
+if USE_CACHE:
+    mmpython.cache_disc(medium)
+    
