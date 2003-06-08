728f3470ff6363f672b893b295a751720393339a
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2003-06-08 13:07:00 +0000

    added header and cleaned up register
    git-svn-id: svn://svn.freevo.org/kaa/trunk@45 a8f5125c-1e01-0410-8897-facf34644b8e

--- mmpython/disc/audioinfo.py
+++ mmpython/disc/audioinfo.py
@@ -1,3 +1,30 @@
+#if 0 /*
+# -----------------------------------------------------------------------
+# audioinfo.py - support for audio cds
+# -----------------------------------------------------------------------
+# $Id$
+#
+# -----------------------------------------------------------------------
+# Copyright (C) 2003 Thomas Schueppel, Dirk Meyer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# ----------------------------------------------------------------------- */
+#endif
+
+
 import mediainfo
 import DiscID
 import CDDB
@@ -50,6 +77,4 @@ class AudioInfo(mediainfo.DiscInfo):
     
         
 factory = mediainfo.get_singleton()  
-audioinfo = AudioInfo
-factory.register( 'sudio/cd', mediainfo.DEVICE, mediainfo.TYPE_AUDIO, audioinfo )
-print "audio cd type registered"
+factory.register( 'audio/cd', mediainfo.DEVICE, mediainfo.TYPE_AUDIO, AudioInfo )
