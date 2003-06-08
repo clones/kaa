7c68e52ae670fc352e299e9f867d4848803f12e1
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2003-06-08 13:43:49 +0000

    use mmpython as lib
    git-svn-id: svn://svn.freevo.org/kaa/trunk@51 a8f5125c-1e01-0410-8897-facf34644b8e

--- mmpython/mediatest.py
+++ mmpython/mediatest.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python
 
-import mediainfo
-import audio.ogginfo
-import audio.pcminfo
-import audio.mp3info
-import video.riffinfo
-import video.mpeginfo
-import video.asfinfo
-import video.movinfo
-import image.jpginfo
-import image.pnginfo
-import image.tiffinfo
-
-import disc.dvdinfo
-import disc.vcdinfo
-import disc.audioinfo
-
 import sys
+sys.path.append('..')
 
-m = mediainfo.get_singleton()
-medium = m.create_from_filename(sys.argv[1])
+import mmpython
+
+medium = mmpython.parse(sys.argv[1])
 #medium.expand_keywords()
 if medium:
     print "medium is: %s" % medium.type
