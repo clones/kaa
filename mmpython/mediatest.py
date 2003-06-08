4e10255f353489085289d05829f4f0ab9f7af4ed
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2003-06-08 16:53:57 +0000

    support a list of files as input
    git-svn-id: svn://svn.freevo.org/kaa/trunk@65 a8f5125c-1e01-0410-8897-facf34644b8e

--- mmpython/mediatest.py
+++ mmpython/mediatest.py
@@ -5,21 +5,25 @@ sys.path.append('..')
 
 import mmpython
 
-USE_CACHE = 1
+USE_CACHE = 0
 
 if USE_CACHE:
     mmpython.use_cache('/tmp/')
 
-medium = mmpython.parse(sys.argv[1])
+for file in sys.argv[1:]:
+    medium = mmpython.parse(file)
+    print "filename : %s" % file
 
-#medium.expand_keywords()
-if medium:
-    print "medium is: %s" % medium.type
-    print medium
-else:
-    print "No Match found"
+    #medium.expand_keywords()
+    if medium:
+        print "medium is: %s" % medium.type
+        print medium
+        print
+        print
+    else:
+        print "No Match found"
 
 
-if USE_CACHE:
-    mmpython.cache_disc(medium)
+    if USE_CACHE:
+        mmpython.cache_disc(medium)
     
