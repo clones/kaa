7953b6f5aaecf9b058e5eabaf1b1f99af50e5c11
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2004-09-11 16:29:33 +0000

    version update
    git-svn-id: svn://svn.freevo.org/kaa/trunk@375 a8f5125c-1e01-0410-8897-facf34644b8e

--- mmpython/version.py
+++ mmpython/version.py
@@ -2,7 +2,7 @@
 # for the program using this lib if it's an up-to-date version
 
 # "offical" version of mmpython
-VERSION = '0.4.5'
+VERSION = '0.4.6'
 
 # latest major change (date as integer)
 CHANGED = 20040629
