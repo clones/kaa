8f95d11e3724b59147848d7dd1a603f7e8e2436f
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-05-07 11:49:35 +0000

    adjust to new codebase
    git-svn-id: svn://svn.freevo.org/kaa/trunk@414 a8f5125c-1e01-0410-8897-facf34644b8e

--- mmpython/setup.py
+++ mmpython/setup.py
@@ -6,7 +6,8 @@ __revision__ = "$Id$"
 
 from distutils.core import setup, Extension
 import popen2
-import version
+
+from mmpython import version
 
 extensions = [ Extension('mmpython/disc/cdrom', ['disc/cdrommodule.c']) ]
 # check for libdvdread (bad hack!)
@@ -16,19 +17,18 @@ try:
     if child.fromchild.readline().find('cannot find') == -1:
         # gcc failed, but not with 'cannot find', so libdvd must be
         # somewhere (I hope)
-        extensions.append(Extension('mmpython/disc/ifoparser', ['disc/ifomodule.c'],
-                                    libraries=[ 'dvdread' ], 
+        extensions.append(Extension('mmpython/disc/ifoparser',
+                                    ['disc/ifomodule.c'],
+                                    libraries=[ 'dvdread' ],
                                     library_dirs=['/usr/local/lib'],
                                     include_dirs=['/usr/local/include']))
     child.wait()
 except AttributeError, e:
     print "No Popen4 found. This seems to be Windows."
     print "Installing without libdvdread support."
-    # Hack: disable extensions for Windows. 
+    # Hack: disable extensions for Windows.
     # This would better be done by a clean detect of windows. But how?
     extensions = []
-    
-    
 
 setup (# Distribution meta-data
        name = "mmpython",
@@ -39,19 +39,20 @@ setup (# Distribution meta-data
        url = "http://mmpython.sf.net",
 
        scripts     = [ 'mminfo' ],
-       package_dir = {'mmpython.video': 'video',
-                      'mmpython.audio': 'audio',
-                      'mmpython.audio.eyeD3': 'audio/eyeD3',
-                      'mmpython.image': 'image',
-                      'mmpython.disc' : 'disc',
-                      'mmpython.misc' : 'misc',
-                      'mmpython': ''},
 
-       packages = [ 'mmpython', 'mmpython.video', 'mmpython.audio', 'mmpython.audio.eyeD3',
-                    'mmpython.image', 'mmpython.disc', 'mmpython.misc' ],
-       
+       package_dir = {'mmpython.video': 'mmpython/video',
+                      'mmpython.audio': 'mmpython/audio',
+                      'mmpython.audio.eyeD3': 'mmpython/audio/eyeD3',
+                      'mmpython.image': 'mmpython/image',
+                      'mmpython.disc' : 'mmpython/disc',
+                      'mmpython.misc' : 'mmpython/misc',
+                      'mmpython': 'mmpython'},
+
+       packages = [ 'mmpython', 'mmpython.video', 'mmpython.audio',
+                    'mmpython.audio.eyeD3', 'mmpython.image',
+                    'mmpython.disc', 'mmpython.misc' ],
+
        # Description of the modules and packages in the distribution
        ext_modules = extensions
-                       
-      )
 
+      )
