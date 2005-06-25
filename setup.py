3637354291ccf2e3b51e03b0be671dc7bc32eacb
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-06-25 10:44:49 +0000

    add global setup.py
    git-svn-id: svn://svn.freevo.org/kaa/trunk@430 a8f5125c-1e01-0410-8897-facf34644b8e

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import distutils.core
+
+for a in sys.argv:
+    if a.startswith('--help'):
+        distutils.core.setup(name="kaa", version="0.1")
+        sys.exit(0)
+
+for submodule in [ 'imlib2', 'thumb' ]:
+    print '[setup] Entering kaa submodule', submodule
+    os.chdir(submodule)
+    execfile('setup.py')
+    os.chdir('..')
+    print '[setup] Leaving kaa submodule', submodule
