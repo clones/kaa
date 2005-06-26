a43ff8731fa6656f16acbac4651a4e2698b50732
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-06-26 11:02:36 +0000

    enhance clean
    git-svn-id: svn://svn.freevo.org/kaa/trunk@433 a8f5125c-1e01-0410-8897-facf34644b8e

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ import os
 import sys
 import distutils.core
 
+submodules = [ 'imlib2', 'thumb', 'epg' ]
+
 for a in sys.argv:
     if a.startswith('--help'):
         distutils.core.setup(name="kaa", version="0.1")
         sys.exit(0)
 
-for submodule in [ 'imlib2', 'thumb', 'epg' ]:
-    print '[setup] Entering kaa submodule', submodule
-    os.chdir(submodule)
+for m in submodules:
+    print '[setup] Entering kaa submodule', m
+    os.chdir(m)
     execfile('setup.py')
     os.chdir('..')
-    print '[setup] Leaving kaa submodule', submodule
+    print '[setup] Leaving kaa submodule', m
+
+if sys.argv[1] == 'clean' and len(sys.argv) == 2:
+    for m in submodules:
+        build = os.path.join(m, 'build')
+        if os.path.isdir(build):
+            print 'removing %s' % build
+            os.system('rm -rf %s' % build)
