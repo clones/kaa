ffcb1133b93266684537a026a02a4347305bb7df
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-07-24 15:44:03 +0000

    a small example
    git-svn-id: svn://svn.freevo.org/kaa/trunk@586 a8f5125c-1e01-0410-8897-facf34644b8e

--- record/test/recorder.py
+++ record/test/recorder.py
@@ -1,4 +1,22 @@
-from kaa.record.dvb import DvbDevice
+import sys
+
+import kaa
+from kaa.notifier import OneShotTimer
+from kaa.record import DvbDevice, Filewriter
 
 dvb = DvbDevice('/dev/dvb/adapter0', '/home/dmeyer/.freevo/channels.conf', 9)
+
+# print some debug
 print dvb.get_card_type()
+print dvb.get_bouquet_list()
+
+# start recording
+id = dvb.start_recording('ZDF', Filewriter('foo.mpg', 0, Filewriter.FT_MPEG))
+
+# stop record after 10 seconds
+t = OneShotTimer(dvb.stop_recording, id).start(10000)
+
+# stop test after 15 seconds
+t = OneShotTimer(sys.exit, 0).start(15000)
+
+kaa.main()
