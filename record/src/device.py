9fc4ffe9c8b411a7c83afc7b5705ff87591d02bc
dischi <dischi@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-09-12 19:26:59 +0000

    add some docs
    git-svn-id: svn://svn.freevo.org/kaa/trunk@766 a8f5125c-1e01-0410-8897-facf34644b8e

--- record/src/device.py
+++ record/src/device.py
@@ -4,6 +4,8 @@
 # -----------------------------------------------------------------------------
 # $Id$
 #
+# This file defines the possible devices for recording.
+#
 # -----------------------------------------------------------------------------
 # kaa-record - A recording module
 # Copyright (C) 2005 Sönke Schwardt, Dirk Meyer
@@ -42,11 +44,36 @@ from ivtv_tuner import IVTV
 # get logging object
 log = logging.getLogger('record')
 
-class DvbDevice(object):
+class Device(object):
     """
-    Wrapper for DVB devices.
+    Basic device class defining the needed functions all devices need to
+    provide. Override all the functions without calling this class.
+    """
+    def start_recording(self, channel, filter_chain):
+        """
+        Start recording the channel to the filter chain. This function needs
+        to return a unique id for stopping the recording later.
+        """
+        raise RuntimeError('start_recording undefined')
+
+
+    def stop_recording(self, id):
+        """
+        Stop the recording with the given id.
+        """
+        raise RuntimeError('stop_recording undefined')
+
+
+class DvbDevice(Device):
+    """
+    Recorder for DVB devices based on the C++ module _DvbDevice.
     """
     def __init__(self, device, channels):
+        """
+        Init the device by creating a C++ object for DVB and create a
+        SocketDispatcher for the file descriptor. The C++ objecty will
+        register and unregister from notifier.
+        """
         self._device = _DvbDevice(device, channels);
         # create socket dispatcher
         sd = SocketDispatcher(self._device.read_fd_data)
@@ -55,19 +82,31 @@ class DvbDevice(object):
 
 
     def start_recording(self, channel, filter_chain):
+        """
+        Start recording a channel to the filter chain. The filter chain needs
+        the video and audio ids to know what part of the transport stream
+        should be recorded. This functions gets all ids for the channel and
+        starts the recording at C++ level.
+        """
         pids = self._device.get_pids(channel)
         log.info("start recording %s with pid list %s" % (channel, pids))
         filter_chain.set_pids(pids[0][0], pids[1][0])
         return self._device.start_recording(channel, filter_chain)
-        
+
+
     def __getattr__(self, attr):
+        """
+        Support get_card_type, get_bouquet_list and stop_recording by passing
+        the call to the C++ object.
+        """
         if attr in ('get_card_type', 'get_bouquet_list',
                     'stop_recording'):
             return getattr(self._device, attr)
         return object.__getattr__(self, attr)
 
 
-class IVTVDevice(IVTV):
+
+class IVTVDevice(Device, IVTV):
     """
     Class for IVTV devices.
     I'm still contemplating weather to keep this object extending IVTV
@@ -77,14 +116,15 @@ class IVTVDevice(IVTV):
                  custom_frequencies=None, resolution=None, aspect=2,
                  audio_bitmask=None, bframes=None, bitrate_mode=1,
                  bitrate=4500000, bitrate_peak=4500000, dnr_mode=None,
-                 dnr_spatial=None, dnr_temporal=None, dnr_type=None, framerate=None,
-                 framespergop=None, gop_closure=1, pulldown=None, stream_type=14):
+                 dnr_spatial=None, dnr_temporal=None, dnr_type=None,
+                 framerate=None, framespergop=None, gop_closure=1,
+                 pulldown=None, stream_type=14):
 
         IVTV.__init__(self, device, norm, chanlist, card_input,
                       custom_frequencies, resolution, aspect,
                       audio_bitmask, bframes, bitrate_mode,
-                      bitrate, bitrate_peak, dnr_mode, dnr_spatial, 
-                      dnr_temporal, dnr_type, framerate, framespergop, 
+                      bitrate, bitrate_peak, dnr_mode, dnr_spatial,
+                      dnr_temporal, dnr_type, framerate, framespergop,
                       gop_closure, pulldown, stream_type)
 
 
@@ -99,10 +139,8 @@ class IVTVDevice(IVTV):
         self.assert_settings()
         self.set_gop_end()
         self.setchan(str(channel))
-        
 
-    def stop_recording(self, channel):
-        log.info("stop recording channel %s" % channel)
+
+    def stop_recording(self, id):
+        log.info("stop recording %s" % id)
         self.stop_encoding()
-        
-
