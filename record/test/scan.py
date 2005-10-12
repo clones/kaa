6c8fc6929d2a6cf7e5bc357ce0264a1609863cd3
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-10-12 19:04:52 +0000

    make it a test app to fill the db
    git-svn-id: svn://svn.freevo.org/kaa/trunk@857 a8f5125c-1e01-0410-8897-facf34644b8e

--- record/test/scan.py
+++ record/test/scan.py
@@ -6,10 +6,18 @@ import kaa.record.v4l_frequencies
 from kaa.record.channels import ALL_CHANNELS
 import kaa.record._vbi as vbi
 
-DEVICE   = '/dev/video0'
-VBI_DEV  = '/dev/vbi0'
-NORM     = 'pal'
-CHANLIST = 'europe-west'
+try:
+    DEVICE, NORM, CHANLIST = sys.argv[1:]
+except:
+    print 'usage %s device norm chanlist' % sys.argv[0]
+    print 'where'
+    print '  device is the video device (e.g. /dev/video0)'
+    print '  norm is "pal" or "ntsc"'
+    print '  chanlist is one of \n    %s' % \
+          '\n    '.join(kaa.record.v4l_frequencies.CHANLIST.keys())
+    sys.exit(0)
+
+VBI_DEV = DEVICE.replace('/video', '/vbi')
 
 x = kaa.record.v4l_tuner.V4L(DEVICE, NORM, CHANLIST)
 x.setinput(0)
@@ -39,20 +47,26 @@ for k in frequencies:
                 for chanid, v4lid, dvbid in ALL_CHANNELS:
                     if data.network[1] in v4lid:
                         print chanid
+                        status = 'found in db'
                         break
                 else:
                     print 'unkown network "%s" (%s)' % data.network
                     chanid = data.network[0]
+                    status = 'found in vbi %s' % data.network[1]
                 break
         else:
             print 'unkown network "%s"' % k
+            status = 'not identified'
             chanid = k
         freq = kaa.record.v4l_frequencies.get_frequency(k, CHANLIST)
-        channels.append((freq, chanid))
+        channels.append((freq, chanid, status))
     else:
         print 'no channel'
 
 print
 print 'Found the following channels:'
-for freq, name in channels:
-    print '  %20s on freq %s' % (name, freq)
+for freq, name, status in channels:
+    print '  %20s on freq %s (%s)' % (name, freq, status)
+print
+print 'Please check if you get correct names and report the result'
+print 'back to the freevo-devel mailing list. Thank you.'
