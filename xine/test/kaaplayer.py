dfb2808413389bf510813eca4bb3365ca71f91a3
tack <tack@a8f5125c-1e01-0410-8897-facf34644b8e>
2005-12-28 14:22:30 +0000

    Several changes to example; syncs to my local tree.
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1065 a8f5125c-1e01-0410-8897-facf34644b8e

--- xine/test/kaaplayer.py
+++ xine/test/kaaplayer.py
@@ -19,8 +19,10 @@ def say(line):
 
 def handle_lirc_event(code, stream, window):
     lirc_map = { "exit": "q", "menu": "m", "select": "space",
-                 "ch-": "[", "ch+": "]", "input": "d",
-                 "up": "up", "down": "down", "left": "left", "right": "right"
+                 "ch-": "[", "ch+": "]", "input": "p",
+                 "up": "up", "down": "down", "left": "left", "right": "right",
+                 "skip": "n", "pause": "pause", "ffwd": ">", "rew": "<",
+                 "play": "play"
                 }
 
     code = code.lower()
@@ -48,13 +50,46 @@ def handle_keypress_event(key, stream, window):
             say("\nDeinterlacing OFF\n")
         else:
             say("\nDeinterlacing ON\n")
-    elif key == "t":
-        pulldown = 1 - deint_post.get_parameters()["pulldown"]
-        deint_post.set_parameters(pulldown = pulldown)
-        if pulldown:
-            say("\nPulldown OFF\n")
+    elif key == "D":
+        method = deint_post.get_parameters()["method"]
+        methods = ["Linear", "GreedyH", "TomsMoComp", "Linear"]
+        deint_post.set_parameters(method = methods[methods.index(method)+1])
+        #deint_post.set_parameters(method = {"GreedyH": "TomsMoComp", "TomsMoComp": "GreedyH"}[method])
+        say("\nDeinterlacing method: %s\n" % deint_post.get_parameters()["method"])
+    elif key == "c":
+        cheap = deint_post.get_parameters()["cheap_mode"]
+        deint_post.set_parameters(cheap_mode = not cheap)
+        if cheap:
+            say("\nCheap mode OFF\n")
         else:
+            say("\nCheap mode ON\n")
+    elif key == "C":
+        frame = deint_post.get_parameters()["framerate_mode"]
+        deint_post.set_parameters(framerate_mode = {"full": "half_bottom", "half_bottom": "full"}[frame])
+        if frame == "full":
+            say("\nHALF framerate mode\n")
+        else:
+            say("\nFULL framerate mode\n")
+
+    elif key == "t":
+        pulldown = deint_post.get_parameters()["pulldown"]
+        deint_post.set_parameters(pulldown = {"none": "vektor", "vektor":"none"}[pulldown])
+        if pulldown == "none":
             say("\nPulldown ON\n")
+        else:
+            say("\nPulldown OFF\n")
+
+    if key == "pause":
+        stream.set_parameter(xine.PARAM_SPEED, xine.SPEED_PAUSE)
+    elif key == "play":
+        stream.set_parameter(xine.PARAM_SPEED, xine.SPEED_NORMAL)
+    elif key == ">":
+        stream.set_parameter(xine.PARAM_SPEED, xine.SPEED_FAST_4)
+    elif key == "<":
+        stream.set_parameter(xine.PARAM_SPEED, xine.SPEED_SLOW_4)
+        
+    if key == "n":
+        play_next_queued()
 
     if stream.is_in_menu:
         d = { "up": xine.EVENT_INPUT_UP, "down": xine.EVENT_INPUT_DOWN,
@@ -77,8 +112,14 @@ def handle_keypress_event(key, stream, window):
             stream.set_parameter(xine.PARAM_SPEED, xine.SPEED_PAUSE)
             say("\n ==== PAUSE ====\n")
 
-    if key == "n":
-        play_next_queued()
+    if key == "N":
+        strength = noise.get_parameters()["luma_strength"]
+        if strength > 0:
+            noise.set_parameters(luma_strength = 0, chroma_strength = 0)
+            print "NOISE off"
+        else:
+            noise.set_parameters(luma_strength = 8, chroma_strength = 5)
+            print "NOISE on"
 
 
 def play_next_queued():
@@ -89,16 +130,15 @@ def play_next_queued():
 
     stream.stop()
     file = play_files.pop(0)
-    # Make argment into mrl if not already
-    mrl = xine.make_mrl(file)
+    mrl = file
     stream.open(mrl)
 
     if not stream.get_info(xine.STREAM_INFO_HAS_VIDEO):
         if not goom_post:
             goom_post = x.post_init("goom", video_targets = [vo], audio_targets=[ao])
-            if "volnorm" in x.list_post_plugins():
-                volnorm_post = x.post_init("volnorm", audio_targets=[ao])
-                goom_post.get_output("audio out").wire(volnorm_post.get_default_input())
+            #if "volnorm" in x.list_post_plugins():
+            #    volnorm_post = x.post_init("volnorm", audio_targets=[ao])
+            #    goom_post.get_output("audio out").wire(volnorm_post.get_default_input())
         stream.get_audio_source().wire(goom_post.get_default_input())
     else:
         stream.get_audio_source().wire(ao)
@@ -107,6 +147,7 @@ def play_next_queued():
     stream.play()
     xine._debug_show_chain(stream._obj)
     gc.collect()
+    print "Now playing:", file
 
 
 def handle_xine_event(event):
@@ -152,12 +193,15 @@ x = xine.Xine()
 #vo = x.open_video_driver("kaa", passthrough = "xv", window=win)
 #vo = x.open_video_driver("xshm", window = win)
 #vo = x.open_video_driver("sdl", window = win)
-vo = x.open_video_driver("xv", window = win)
+#vo = x.open_video_driver("xv", window = win)
+win._aspect = -1
+vo = x.open_video_driver("xv", wid = win.get_id(), frame_output_cb = notifier.WeakCallback(x._default_frame_output_cb, win), 
+    dest_size_cb = notifier.WeakCallback(x._default_dest_size_cb, win))
 
 ao = x.open_audio_driver()
 stream = x.new_stream(ao, vo)
-stream.is_in_menu = False
 stream.signals["event"].connect(handle_xine_event)
+stream.is_in_menu = False
 
 kaa.signals["idle"].connect_weak(output_status_line, stream, win)
 kaa.signals["stdin_key_press_event"].connect_weak(handle_keypress_event, stream, win)
@@ -169,35 +213,53 @@ if kaa.input.lirc.init():
 
 def handle_resize(old, new, window):
     window.show()
+    vo.send_gui_data(xine.GUI_SEND_VIDEOWIN_VISIBLE, 1)
     window.set_fullscreen()
+
+def configure_event(pos, size, window):
+    vo.send_gui_data(xine.GUI_SEND_DRAWABLE_CHANGED, window.get_id())
+
 
 # Hook the first resize event so we can show the window once we're resized
 # to the proper movie size.
 win.signals["resize_event"].connect_once(handle_resize, win)
+win.signals["configure_event"].connect_weak(configure_event, win)
+
 
 deint_post = x.post_init("tvtime", video_targets = [vo])
-deint_post.set_parameters(method = "GreedyH", enabled = True)
-#print deint_post.get_parameters_desc()
-#print deint_post.get_parameters()
+deint_post.set_parameters(method = "GreedyH", enabled = True, chroma_filter = True)
+#deint_post.set_parameters(method = "TomsMoComp", enabled = True, chroma_filter = True)
 stream.get_video_source().wire(deint_post.get_default_input())
 #deint_post.set_parameters(cheap_mode = True)
 #deint_post.set_parameters(framerate_mode = "half_top")
 
-#expand = x.post_init("expand", video_targets = [vo])
+expand = x.post_init("expand", video_targets = [vo])
 #stream.get_video_source().wire(expand.get_default_input())
 
-#x.set_config_value("effects.goom.fps", 20)
+#noise = x.post_init("noise", video_targets = [vo])
+#deint_post.get_default_output().wire(noise.get_default_input())
+#stream.get_video_source().wire(noise.get_default_input())
+
+x.set_config_value("effects.goom.fps", 20)
 x.set_config_value("effects.goom.width", 512)
 x.set_config_value("effects.goom.height", 384)
 x.set_config_value("effects.goom.csc_method", "Slow but looks better")
 
+eq2_post = x.post_init("eq2", video_targets = [vo])
+eq2_post.set_parameters(gamma = 1.2, contrast = 1.1)
+#noise.get_default_output().wire(eq2_post.get_default_input())
+#stream.get_video_source().wire(eq2_post.get_default_input())
+
+
+#stream.get_video_source().wire(vo)
 goom_post = None
 
 play_next_queued()
 
+kaa.base.create_logger()
 kaa.main()
 win.hide()
 
 # Test garbage collection
-del stream, ao, vo, x, win, goom_post, deint_post
+del stream, ao, vo, x, win, goom_post, deint_post, expand, eq2_post
 gc.collect()
