c2b8f0747f0e185710c2f1f7887f2a92ad3c79b1
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-02-17 21:41:44 +0000

    add basic cache client to populate the db
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1189 a8f5125c-1e01-0410-8897-facf34644b8e

---
+++ WIP/vfs/test/cache.py
@@ -0,0 +1,48 @@
+import sys
+import kaa
+import kaa.vfs.client
+
+c = kaa.vfs.client.Client('vfsdb')
+
+checked  = []
+to_check = []
+
+def progress(cur, total, item):
+    n = 0
+    if total > 0:
+        n = int((cur / float(total)) * 50)
+    sys.stdout.write("|%51s| %d / %d\r" % (("="*n + ">").ljust(51), cur, total))
+    sys.stdout.flush()
+    if cur == total:
+        print
+
+def check():
+    if to_check:
+        d = to_check.pop(0)
+        checked.append(d)
+        print d.filename
+        q = d.listdir()
+        q.signals['up-to-date'].connect(next, q)
+        q.signals['progress'].connect(progress)
+        q.monitor()
+    else:
+        sys.exit(0)
+    
+def next(q):
+    q.monitor(False)
+    for f in q:
+        if f._vfs_isdir:
+            for i in checked + to_check:
+                if f.filename == i.filename:
+                    break
+            else:
+                to_check.insert(0, f)
+    check()
+    
+
+d = c.get(sys.argv[1])
+to_check.append(d)
+
+check()
+
+kaa.main()
