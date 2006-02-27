592e21f74a038f980926b9040c7b0edb6fadadf8
rshortt <rshortt@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-02-27 12:27:52 +0000

    Add the ability to use inet socket as well.
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1236 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/epg2/src/__init__.py
+++ WIP/epg2/src/__init__.py
@@ -1,31 +1,45 @@
 import os
 import logging
+from socket import gethostbyname, gethostname
 
 from kaa.base import ipc
 from client import *
 from server import *
 
-__all__ = [ 'connect' ]
+__all__ = [ 'connect', 'DEFAULT_EPG_PORT', 'GuideClient', 'GuideServer' ]
 
 # connected client object
 _client = None
 
-def connect(epgdb, logfile='/tmp/kaa-epg.log', loglevel=logging.INFO):
+def connect(epgdb, address='127.0.0.1', logfile='/tmp/kaa-epg.log', loglevel=logging.INFO):
     """
     """
     global _client
 
-    # get server filename
-    server = os.path.join(os.path.dirname(__file__), 'server.py')
+    if _client:
+        return _client
 
-    if epgdb.find(':') >= 0:
+    if address.split(':')[0] not in ['127.0.0.1', '0.0.0.0'] and \
+       address != gethostbyname(gethostname()):
         # epg is remote:  host:port
-        # TODO: create socket, pass it to client
-        _client = GuideClient("epg")
+        if address.find(':') >= 0:
+            host, port = address.split(':', 1)
+        else:
+            host = address
+            port = DEFAULT_EPG_PORT
+
+        # create socket, pass it to client
+        _client = GuideClient((host, port))
 
     else:
-        # epg is local
-        _client = ipc.launch([server, logfile, str(loglevel), epgdb], 2, GuideClient, "epg")
+        # EPG is local, only use unix socket
+
+        # get server filename
+        server = os.path.join(os.path.dirname(__file__), 'server.py')
+
+        _client = ipc.launch([server, logfile, str(loglevel), epgdb, address], 
+                              2, GuideClient, "epg")
+
     return _client
 
 
