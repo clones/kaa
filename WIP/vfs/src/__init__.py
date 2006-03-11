0a30f402b178d9fc0cf59d76d4e8fbfd1be38534
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-03-11 19:49:57 +0000

    rename vfsd.py back to to server.py
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1276 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/vfs/src/__init__.py
+++ WIP/vfs/src/__init__.py
@@ -62,7 +62,7 @@ def connect(vfsdb, logfile=None, loglevel=logging.INFO):
     if not logfile:
         logfile = os.path.join(vfsdb, 'log')
     # get server filename
-    server = os.path.join(os.path.dirname(__file__), 'vfsd.py')
+    server = os.path.join(os.path.dirname(__file__), 'server.py')
 
     _client = ipc.launch([server, logfile, str(loglevel)], 5, Client, vfsdb)
     return _client
