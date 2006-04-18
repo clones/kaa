61c41008f09b3897198cc82c52011e5260b48dcf
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-04-18 20:04:05 +0000

    improve generic player
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1410 a8f5125c-1e01-0410-8897-facf34644b8e

--- player/src/generic.py
+++ player/src/generic.py
@@ -6,14 +6,33 @@ class Player(object):
     def __init__(self):
         object.__setattr__(self, 'player', None)
 
+        object.__setattr__(self, 'signals',  {
+            "pause": notifier.Signal(),
+            "play": notifier.Signal(),
+            "pause_toggle": notifier.Signal(),
+            "seek": notifier.Signal(),
+            "open": notifier.Signal(),
+            "start": notifier.Signal(),
+            # Stream ended (either stopped by user or finished)
+            "end": notifier.Signal(),
+            "stream_changed": notifier.Signal(),
+            "frame": notifier.Signal(), # CAP_CANVAS
+            "osd_configure": notifier.Signal(),  # CAP_OSD
+            # Process is about to die (shared memory will go away)
+            "quit": notifier.Signal()
+            })
+
     def open(self, mrl):
-        if self.player:
-            self.player.stop()
-            self.player = None
+        if self.player and self.player.get_state() \
+               not in (STATE_NOT_RUNNING, STATE_IDLE):
+            raise PlayerError('player is running')
         cls = get_player_class(mrl)
         if not cls:
             raise PlayerError("No supported player found to play %s", mrl)
+        # if not self.player or cls.get_player_id() != self.player.get_player_id():
         object.__setattr__(self, 'player', cls())
+        for signal in self.signals.keys():
+            self.player.signals[signal].connect_weak(self.signals[signal].emit)
         return self.player.open(mrl)
         
     def __getattr__(self, attr):
