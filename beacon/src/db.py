85a9e282f98d830c412bb1b1aac36e006f52a85d
tack <tack@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-04-23 02:38:09 +0000

    Bug!  ping dischi :)
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1481 a8f5125c-1e01-0410-8897-facf34644b8e

--- beacon/src/db.py
+++ beacon/src/db.py
@@ -238,6 +238,7 @@ class Database(object):
                 # delete items and all subitems from the db. The delete function
                 # will return all ids deleted, callbacks are not allowed, so
                 # we can just continue
+                # FIXME: 'id' here is wrong; what's it supposed to be?
                 changed_id.extend(self._delete(id))
                 continue
             if function == 'update':
