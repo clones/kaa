f2ed8e2f9cad5d26e0b4a069d9e87c1c868b8365
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-06-24 17:41:10 +0000

    make it possible to search in more than one channel
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1559 a8f5125c-1e01-0410-8897-facf34644b8e

--- epg/src/client.py
+++ epg/src/client.py
@@ -121,18 +121,19 @@ class Client(object):
         Convert raw search result data from the server into python objects.
         """
         results = []
+        channel = None
         for row in query_data:
-            if row[0] not in self._channels_by_db_id:
-                continue
-            channel = self._channels_by_db_id[row[0]]
-            program = Program(channel, *row[2:])
-            results.append(program)
+            if not channel or row[0] != channel.db_id:
+                if row[0] not in self._channels_by_db_id:
+                    continue
+                channel = self._channels_by_db_id[row[0]]
+            results.append(Program(channel, *row[2:]))
         if callback:
             callback(results)
         return results
 
 
-    def search(self, callback=None, **kwargs):
+    def search(self, callback=None, channel=None, time=None, **kwargs):
         """
         Search the db. This will call the search function on server side using
         kaa.ipc. Notice: this will call kaa.notifier.step() until the result
@@ -141,29 +142,24 @@ class Client(object):
         if self.status == DISCONNECTED:
             return []
 
-        if "channel" in kwargs:
-            ch = kwargs["channel"]
-            if type(ch) == Channel:
-                kwargs["channel"] = ch.db_id
-            elif type(ch) == tuple and len(ch) == 2:
-                kwargs["channel"] = kaa.db.QExpr("range", (ch[0].db_id, ch[1].db_id))
-            else:
-                # FIXME: this is ugly. Why not a longer list?
-                raise ValueError, "channel must be Channel object"
+        if channel is not None:
+            if isinstance(channel, Channel):
+                kwargs["channel"] = channel.db_id
+            if isinstance(channel, (tuple, list)):
+                kwargs["channel"] = [ c.db_id for c in channel ]
 
-        if "time" in kwargs:
-            if type(kwargs["time"]) in (int, float, long):
+        if time is not None:
+            if isinstance(time, (int, float, long)):
                 # Find all programs currently playing at the given time.  We
                 # add 1 second as a heuristic to prevent duplicates if the
                 # given time occurs on a boundary between 2 programs.
-                start, stop = kwargs["time"] + 1, kwargs["time"] + 1
+                start, stop = time + 1, time + 1
             else:
-                start, stop = kwargs["time"]
+                start, stop = time
 
             max = self._max_program_length
             kwargs["start"] = kaa.db.QExpr("range", (int(start) - max, int(stop)))
-            kwargs["stop"] = kaa.db.QExpr(">=", int(start))
-            del kwargs["time"]
+            kwargs["stop"]  = kaa.db.QExpr(">=", int(start))
 
         if callback:
             rpc = self.server.rpc('guide.query', self._program_rows_to_objects, callback)
@@ -227,20 +223,6 @@ class Client(object):
         if tuner_id not in self._channels_by_tuner_id:
             return None
         return self._channels_by_tuner_id[tuner_id]
-
-
-    def get_max_program_length(self):
-        """
-        Get maximum program length
-        """
-        return self._max_program_length
-
-
-    def get_num_programs(self):
-        """
-        Get number of programs in the db.
-        """
-        return self._num_programs
 
 
     def get_channels(self):
