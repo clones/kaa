9166bb027474fe7b6b4358298beeca0ac83e83c8
tack <tack@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-07-09 00:20:12 +0000

    BeaconFS class using FUSE.
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1599 a8f5125c-1e01-0410-8897-facf34644b8e

---
+++ beacon/src/fusefs.py
@@ -0,0 +1,125 @@
+# python imports
+import os
+import sys
+import time
+import logging
+import stat
+import fuse
+import errno
+
+import kaa.notifier
+import kaa.beacon
+import kaa.utils
+
+# get logging object
+log = logging.getLogger('beacon')
+
+
+class MyStat(fuse.Stat):
+    def __init__(self):
+        self.st_mode = 0
+        self.st_ino = 0
+        self.st_dev = 0
+        self.st_nlink = 0
+        self.st_uid = 0
+        self.st_gid = 0
+        self.st_size = 0
+        self.st_atime = 0
+        self.st_mtime = 0
+        self.st_ctime = 0
+
+
+class BeaconFS(fuse.Fuse):
+    def __init__(self, mountpoint, query, *args, **kw):
+        self._query = query
+        self._filename_map = {}
+        self._query_update_time = 0
+        query.signals["changed"].connect_weak(self._query_changed)
+        query.monitor()
+        
+        fuse.Fuse.__init__(self, *args, **kw)
+
+        self.multithreaded = 1
+        self.parse(args=[mountpoint, "-f"])
+
+
+    def _query_changed(self):
+        self._filename_map = {}
+        self._query_update_time = int(time.time())
+        for item in self._query:
+            name = item.getattr('name')
+            if name in self._filename_map:
+                n = 1
+                file, ext = os.path.splitext(name)
+                while name in self._filename_map:
+                    name = "%s-%d%s" % (file, n, ext)
+                    n += 1
+    
+            self._filename_map[name] = item
+        
+
+    def getattr(self, path):
+        st = MyStat()
+        if path == '/':
+            st.st_mode = stat.S_IFDIR | 0755
+            st.st_nlink = 2
+            st.st_atime = st.st_mtime = st.st_ctime = self._query_update_time
+            return st
+
+        file = path[1:] # assume prefixed with single /
+        if file in self._filename_map:
+            item = self._filename_map[file]
+            st.st_mode = stat.S_IFREG | 0444 | stat.S_IFLNK
+            st.st_nlink = 1
+            st.st_size = len(item.filename)
+            st.st_atime = st.st_mtime = st.st_ctime = item.getattr('mtime')
+            return st
+        else:
+            return -errno.ENOENT
+    
+
+    def readdir(self, path, offset):
+        for r in  '.', '..':
+            yield fuse.Direntry(r)
+        for file in self._filename_map:
+            yield fuse.Direntry(file)
+
+
+    def readlink(self, path):
+        file = path[1:] # assume prefixed with single /
+        if file not in self._filename_map:
+            return -errno.ENOENT
+        return self._filename_map[file].filename
+
+
+    def check(self):
+        # Do some sanity checks to catch common gotchas.
+        # Check for kernel support.
+        if not os.path.exists("/dev/fuse"):
+            raise fuse.FuseError, "/dev/fuse not present; fuse module not loaded?"
+
+        # Check that we have write access to /dev/fuse
+        try:
+            file("/dev/fuse", "w")
+        except IOError, (err, msg):
+            if err == 13:
+                raise fuse.FuseError, "No write access to /dev/fuse; check permissions."
+            raise
+
+        # Make sure fusermount is found in path.
+        fusermount = kaa.utils.which("fusermount")
+        if not fusermount:
+            raise fuse.FuseError, "fusermount is not found in PATH."
+
+        # If we're non-root, check that fusermount is suidroot.
+        if os.geteuid() != 0:
+            # This shouldn't fail, because kaa.utils.which already statted it.
+            st = os.stat(fusermount)
+            if st[stat.ST_UID] != 0 or not st[stat.ST_MODE] & stat.S_ISUID:
+                raise fuse.FuseError, "fusermount is not suid root and you're not root."
+           
+
+    def main(self):
+        self.check()
+        fuse.Fuse.main(self)
+        kaa.notifier.MainThreadCallback(kaa.notifier.shutdown)()
