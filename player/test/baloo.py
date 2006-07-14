dc14e7aa29230a08d7b52f21b19aae95638e6e10
tack <tack@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-07-14 02:01:42 +0000

    Make baloo work again; also some UI improvements.
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1685 a8f5125c-1e01-0410-8897-facf34644b8e

--- player/test/baloo.py
+++ player/test/baloo.py
@@ -25,6 +25,7 @@ class Baloo(object):
         dic = { "on_search_activate" : self.on_search_activate,
                 "on_pause_clicked" : self.on_pause_clicked,
                 "on_next_clicked" : self.on_next_clicked,
+                "on_prev_clicked" : self.on_prev_clicked,
                 "on_exit" : self.on_exit }
         self.xml.signal_autoconnect (dic)
 
@@ -32,15 +33,22 @@ class Baloo(object):
         self.search = []
         self.current = None
         self.player = kaa.player.Player()
-        self.player.signals['end'].connect(self.play_next)
+        self.player.signals['end'].connect_weak(self.play_next)
+        self.player.signals['pause'].connect_weak(self.player_pause)
+        self.player.signals['play'].connect_weak(self.player_play)
 
         
     def update_playlist(self):
         if len(self.search):
+            for t in ('pause', 'prev', 'next'):
+                self.xml.get_widget(t).set_sensitive(True)
             return True
+
         for t in ('artist', 'album'):
             self.xml.get_widget(t).set_text('')
         self.xml.get_widget('title').set_text('<no match>')
+        for t in ('pause', 'prev', 'next'):
+            self.xml.get_widget(t).set_sensitive(False)
         if self.timer.active():
             self.timer.stop()
         return True
@@ -48,7 +56,7 @@ class Baloo(object):
     def on_search_activate(self, data=None):
         string = self.xml.get_widget("search").get_text()
         self.search = kaa.beacon.query(type='audio', keywords=string)
-        self.search.signals['updated'].connect(update_playlist)
+        self.search.signals['changed'].connect(self.update_playlist)
         self.search.monitor()
         self.update_playlist()
         if self.current:
@@ -66,6 +74,9 @@ class Baloo(object):
     def on_next_clicked(self, data=None):
         self.player.stop()
 
+    def on_prev_clicked(self, data=None):
+        self.play_next(-1)
+
     def on_exit(self, data=None):
         sys.exit(0)
 
@@ -74,9 +85,9 @@ class Baloo(object):
         self.xml.get_widget('timer').set_text('%02d:%02d' % ((secs / 60), (secs % 60)))
         return True
 
-    def play_next(self):
+    def play_next(self, offset = 1):
         try:
-            index = self.search.index(self.current) + 1
+            index = self.search.index(self.current) + offset
             if index == len(self.search):
                 index = 0
         except (KeyError, ValueError), e:
@@ -85,12 +96,25 @@ class Baloo(object):
         if not len(self.search):
             return False
         self.current = self.search[index]
+        #self.player.signals["open"].connect_once(self.player.play, video=False)
         self.player.open(self.current.filename)
         self.player.play(video=False)
         for t in ('title', 'artist', 'album'):
             label = self.xml.get_widget(t)
             value = self.current.getattr(t) or ''
-            label.set_text(value)
-            
+            if t == 'title':
+                value = "<b>%s</b>" % value
+            label.set_markup(value)
+
+        self.xml.get_widget('prev').set_sensitive(index > 0)
+        self.xml.get_widget('next').set_sensitive(index < len(self.search)-1)
+        self.xml.get_widget('count').set_text("%d of %d" % (index+1, len(self.search)))
+ 
+    def player_pause(self):
+        self.xml.get_widget('pause').set_label('gtk-media-play')
+
+    def player_play(self):
+        self.xml.get_widget('pause').set_label('gtk-media-pause')
+
 baloo = Baloo()
 kaa.main()
