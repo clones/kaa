63ff8b28621cb20b88eec050f74fd62787196071
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-07-14 10:26:31 +0000

    destdirs always exist
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1690 a8f5125c-1e01-0410-8897-facf34644b8e

--- beacon/src/thumbnail/thumbnail.py
+++ beacon/src/thumbnail/thumbnail.py
@@ -124,8 +124,6 @@ class Thumbnail(object):
             return
 
         dest = '%s/%s' % (self.destdir, type)
-        if not os.path.isdir(dest):
-            os.makedirs(dest, 0700)
 
         i = self._thumbnail % type + '.png'
         png(self.name, i, SIZE[type], image._image)
@@ -145,8 +143,6 @@ class Thumbnail(object):
         Thumbnail.next_id += 1
         
         dest = '%s/%s' % (self.destdir, type)
-        if not os.path.isdir(dest):
-            os.makedirs(dest, 0700)
 
         # schedule thumbnail creation
         _client.schedule(Thumbnail.next_id, self.name,
