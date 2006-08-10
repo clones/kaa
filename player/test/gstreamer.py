47f3870ef89bd2ddf7bff514d1a3092e28d9c6c0
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-08-10 19:55:26 +0000

    some findings while playing with gstreamer
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1852 a8f5125c-1e01-0410-8897-facf34644b8e

--- player/test/gstreamer.py
+++ player/test/gstreamer.py
@@ -4,6 +4,8 @@ import gst
 import sys
 import time
 
+import kaa.display
+
 #
 # play an uri like file:///home/foo/bar.mp3
 # works for audio and video files
@@ -20,10 +22,13 @@ import time
 # The good news for kaa.record
 #
 # o We can use the gst pipe code to decode and change the stuff we want
+#   (see multifdsink and all the fd/rtp/udp filter)
 #
 # The bad news:
 #
 # o The doc sucks. What properties are there?
+#   Answer: looks like gst-inspect can help, e.g. to get more information
+#   about xv output call 'gst-inspect-0.10 xvimagesink'
 #
 # o The examples for gst-python 0.10.x are all based on 0.8 and do not work.
 #
@@ -37,11 +42,63 @@ import time
 # http://www.fluendo.com/elisa/ (read the source)
 #
 
+# Create kaa.display window. It looks like gstreamer only has support
+# for X and XV output right now. That sucks, no mga and no dfb.
+window = kaa.display.X11Window(size = (400,600), title = "kaa.player")
+window.show()
+
+vo = gst.element_factory_make("xvimagesink", "vo")
+vo.set_xwindow_id(long(window.get_id()))
+vo.set_property('force-aspect-ratio', True)
+
+# now create the player and set the output
 player = gst.element_factory_make("playbin", "player")
-# change the videosink to be in kaa.display
+player.set_property('video-sink', vo)
+
+# TODO: how to add some extra filter to the chain? An kaa.canvas overlay filter
+# needs to be written (take a look at the pango stuff that is used to write
+# subtitles: textoverlay) and connected. To support videos aspect != window aspect
+# some sort of expand filter is also needed (videobox or videoscale).
+
+# I tried to add a filter similar to the one we need for kaa.canvas. The
+# dicetv effect looks like a nice way to test it. But playbin does not support
+# adding more filter into the video chain (which is type gst.Bin). Other
+# code examples like totel or Elisa use some trick to create a new Bin, use that
+# as video-sink for playbin and add effect + real output in that bin. But I failed
+# to setup and connect the bin.
+
+# Idea
+#
+# | vo_sink = gst.Bin()
+# | effect = gst.element_factory_make("dicetv", "effect")
+# | vo_sink.add(effect)
+# | vo_sink.add(vo)
+# | effect.link(vo)
+#
+# The linking fails when vo has the window id set. No idea why this
+# happens. Ignore that and continue
+#
+# | pad = effect.get_pad('sink')
+# | ghostpad = gst.GhostPad("sink", pad)
+# | vo_sink.add_pad(ghostpad)
+#
+# No idea why the sink pad from effect is the sink pad for vo_sink. But totem
+# does this this way (search for puzzle in the code). So this looks correct
+# but does not work because of set_xwindow_id. Doing that after the linking
+# will freeze gstreamer. So I give up for now. We also need a way to add new
+# effects to add a simple deinterlace.
+# 
+#
+# for audio files, add goom this works. But using the vis-plugin code to
+# inject the effect object does not work. So code for playbin in plugins base
+# has some ascii art how things are plugged together.
+#
+# | goom = gst.element_factory_make("goom", "goom")
+# | player.set_property('vis-plugin', goom)
 
 print 'Playing:', sys.argv[1]
 player.set_property('uri', sys.argv[1])
+
 player.set_state(gst.STATE_PLAYING)
 
 # play 10 sec
