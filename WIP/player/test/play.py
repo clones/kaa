ab2ffaf0fa9d9260fa060449372be6b692c3d54f
tack <tack@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-08-11 18:05:53 +0000

    Only show window once video is playing.
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1865 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/player/test/play.py
+++ WIP/player/test/play.py
@@ -5,9 +5,9 @@ import kaa.display
 import kaa.player
 
 window = kaa.display.X11Window(size = (800,600), title = "kaa.player")
-window.show()
 
 player = kaa.player.Player(window)
+player.signals["start"].connect_once(window.show)
 player.open(sys.argv[1], player='xine')
 player.play()
 
