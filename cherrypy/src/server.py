0e222f0ed7391db980406b5224b28bf10e350174
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-11-01 16:47:39 +0000

    make kaa.cherrypy LGPL
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1956 a8f5125c-1e01-0410-8897-facf34644b8e

--- cherrypy/src/server.py
+++ cherrypy/src/server.py
@@ -11,19 +11,19 @@
 # First Edition: Dirk Meyer <dmeyer@tzi.de>
 # Maintainer:    Dirk Meyer <dmeyer@tzi.de>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version
+# 2.1 as published by the Free Software Foundation.
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
-# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-# Public License for more details.
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
 #
 # -----------------------------------------------------------------------------
 
@@ -148,7 +148,7 @@ def start(Root, *args, **kwargs):
         })
 
     for key, value in config.static.items():
-        cherrypy.config.update( { key: { 
+        cherrypy.config.update( { key: {
             'staticFilter.on': True,
             'staticFilter.dir': value
             } } )
