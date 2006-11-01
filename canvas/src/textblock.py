614b62edf87c5bc8876745c4765fb12b4c1d9120
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-11-01 16:58:45 +0000

    checked all dependencies and make kaa.canvas LGPL
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1957 a8f5125c-1e01-0410-8897-facf34644b8e

--- canvas/src/textblock.py
+++ canvas/src/textblock.py
@@ -1,3 +1,31 @@
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# $Id$
+# -----------------------------------------------------------------------------
+# kaa.canvas - Canvas library based on kaa.evas
+# Copyright (C) 2005, 2006 Jason Tackaberry
+#
+# First Edition: Jason Tackaberry <tack@sault.org>
+# Maintainer:    Jason Tackaberry <tack@sault.org>
+#
+# Please see the file AUTHORS for a complete list of authors.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version
+# 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
+# -----------------------------------------------------------------------------
+
 __all__ = [ 'TextBlock' ]
 
 from object import *
