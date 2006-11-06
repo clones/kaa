0abd7ec0bd4b4ee25e5e45ba75c56c3716e382c4
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-11-06 15:12:53 +0000

    update test app
    git-svn-id: svn://svn.freevo.org/kaa/trunk@1987 a8f5125c-1e01-0410-8897-facf34644b8e

--- popcorn/test/play.py
+++ popcorn/test/play.py
@@ -6,9 +6,11 @@ import kaa.display
 import kaa.popcorn
 import kaa.input.stdin
 
-logging.getLogger('popcorn').setLevel(logging.INFO)
+logging.getLogger('popcorn').setLevel(logging.DEBUG)
 # logging.getLogger('popcorn.child').setLevel(logging.ERROR)
 
+BACKEND = 'mplayer'                     # mplayer, xine, gstreamer
+WINDOW  = '4:3'                         # '4:3', '16:3', 'none'
 
 def print_msg(msg):
     print '>', msg
@@ -29,21 +31,27 @@ def handle_key(key, player):
         win = player.get_window()
         win.set_fullscreen(not win.get_fullscreen())
 
-window = kaa.display.X11Window(size = (800,600), title = "kaa.popcorn")
-
-player = kaa.popcorn.Player(window)
-player.signals["start"].connect_once(window.show)
+if WINDOW == 'none':
+    player = kaa.popcorn.Player()
+else:
+    if WINDOW == '16:9':
+        window = kaa.display.X11Window(size = (800,450), title = "kaa.popcorn")
+    else:
+        window = kaa.display.X11Window(size = (800,600), title = "kaa.popcorn")
+    player = kaa.popcorn.Player(window)
+    player.signals["start"].connect_once(window.show)
+    
 player.signals["start"].connect(print_msg, 'playback started')
 player.signals["end"].connect(print_msg, 'playback end')
-# player.signals["end"].connect(next, 'xine')
+# player.signals["end"].connect(next, BACKEND)
 player.signals["failed"].connect(print_msg, 'playback failed')
 
 kaa.signals["stdin_key_press_event"].connect(handle_key, player)
 if player.get_window():
     player.get_window().signals["key_press_event"].connect(handle_key, player)
 
-kaa.notifier.OneShotTimer(next, 'xine').start(0)
-kaa.notifier.OneShotTimer(player.stop).start(1)
+kaa.notifier.OneShotTimer(next, BACKEND).start(0)
+#kaa.notifier.OneShotTimer(player.stop).start(1)
 
 def print_pos():
     print '\r', player.get_position(),
