a60b92beb92ea14c1c323eb13afc8548048da523
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-11-08 20:01:13 +0000

    time can be None with latest kaa.xine fix
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2009 a8f5125c-1e01-0410-8897-facf34644b8e

--- popcorn/src/backends/xine/player.py
+++ popcorn/src/backends/xine/player.py
@@ -103,7 +103,8 @@ class Xine(MediaPlayer):
 
     def _child_set_status(self, pos, time, length, status, speed):
         old_pos = self._position
-        if self.get_state() in (STATE_PAUSED, STATE_PLAYING, STATE_OPEN):
+        if self.get_state() in (STATE_PAUSED, STATE_PLAYING, STATE_OPEN) and \
+               time is not None:
             self._position = float(time)
         self._streaminfo["length"] = length
 
