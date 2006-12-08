c6d238f8444d3de5a0d1fba9f69f565d046bc991
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2006-12-08 12:50:57 +0000

    use config file for caps in xine and gstreamer
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2185 a8f5125c-1e01-0410-8897-facf34644b8e

--- popcorn/src/backends/gstreamer/__init__.py
+++ popcorn/src/backends/gstreamer/__init__.py
@@ -1,8 +1,43 @@
-from player import GStreamer
+# -*- coding: iso-8859-1 -*-
+# -----------------------------------------------------------------------------
+# gstreamer/__init__.py - gstreamer backend
+# -----------------------------------------------------------------------------
+# $Id$
+#
+# -----------------------------------------------------------------------------
+# kaa.popcorn - Generic Player API
+# Copyright (C) 2006 Jason Tackaberry, Dirk Meyer
+#
+# Please see the file AUTHORS for a complete list of authors.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MER-
+# CHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+# -----------------------------------------------------------------------------
+
+# kaa.popcorn imports
 from kaa.popcorn.backends import register
 from kaa.popcorn.ptypes import *
 
+# player imports
+from player import GStreamer
+from config import config
+
 def get_capabilities():
+    """
+    Return capabilities of the gstreamer backend.
+    """
     capabilities = {
         CAP_CANVAS : False,
         CAP_CANVAS : False,
@@ -10,18 +45,19 @@ def get_capabilities():
         CAP_VARIABLE_SPEED : False,
         CAP_VISUALIZATION : True,
 
-        CAP_DVD : 0,
-        CAP_DVD_MENUS : 0,
-        CAP_DEINTERLACE : 8
+        CAP_DVD : config.capability.dvd,
+        CAP_DVD_MENUS : config.capability.dvdmenu,
+        CAP_DEINTERLACE : config.capability.deinterlace
     }
+
     schemes = [ "file", "fifo", "dvd", "vcd", "cdda", "http", "tcp", "udp",
                 "rtp", "smb", "mms", "pnm", "rtsp" ]
 
     # list of extentions when to prefer this player
-    exts = ["mpg", "mpeg", "iso"]
+    exts = config.preferred.extentions.split(' ')
 
     # list of codecs when to prefer this player
-    codecs = []
+    codecs = config.preferred.codecs.split(' ')
 
     return capabilities, schemes, exts, codecs
 
