c2c4db643b81b369e66dcb39203f41836dd6c4b7
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-01-16 13:07:01 +0000

    add gtk video test, does not work with xine
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2404 a8f5125c-1e01-0410-8897-facf34644b8e

---
+++ popcorn/test/gtkvideo.py
@@ -0,0 +1,17 @@
+import sys
+import logging
+import kaa.popcorn
+import kaa.display
+    
+# logging.getLogger('popcorn').setLevel(logging.DEBUG)
+
+window = kaa.display.GladeWindow('gtkvideo.glade', 'window1')
+video  = window.get_widget('video')
+vidwin = kaa.display.GTKWindow(video.window)
+
+player = kaa.popcorn.Player()
+kaa.popcorn.config.preferred = 'mplayer'
+player.set_window(vidwin)
+player.open(sys.argv[1])
+player.play()
+kaa.main()
