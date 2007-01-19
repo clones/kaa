dd72a67010291b8f6d05ed8d0576b8834d2914bc
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-01-19 14:51:00 +0000

    set colorkey to black
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2425 a8f5125c-1e01-0410-8897-facf34644b8e

--- popcorn/src/backends/mplayer/player.py
+++ popcorn/src/backends/mplayer/player.py
@@ -408,7 +408,7 @@ class MPlayer(MediaPlayer):
         if isinstance(self._window, kaa.display.X11Window):
             wid = "0x%x" % self._window.get_id()
             display = self._window.get_display().get_string()
-            args.add(vo='xv', wid=wid, display=display)
+            args.add(vo='xv', wid=wid, display=display, colorkey='0x000000')
         else:
             # FIXME: add support for DFB/FB/etc
             args.add(vo='null')
