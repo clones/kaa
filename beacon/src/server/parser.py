2541638df8521d94dd82b0280f0d73e6c9bb1ffc
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-04-18 17:44:50 +0000

    better imlib2 error handling
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2635 a8f5125c-1e01-0410-8897-facf34644b8e

--- beacon/src/server/parser.py
+++ beacon/src/server/parser.py
@@ -208,7 +208,7 @@ def parse(db, item, store=False, check_image=False):
                     produced_load = 2
                     img = kaa.imlib2.open_from_memory(metadata.get('thumbnail'))
                     t.set(img, thumbnail.NORMAL)
-                except Exception:
+                except (ValueError, IOError):
                     log.error('image thumbnail')
 
     else:
@@ -225,7 +225,7 @@ def parse(db, item, store=False, check_image=False):
                 try:
                     produced_load = 2
                     t.image = kaa.imlib2.open_from_memory(metadata['thumbnail'])
-                except ValueError:
+                except (ValueError, IOError):
                     log.error('raw thumbnail')
 
         for ext in ('.jpg', '.png'):
