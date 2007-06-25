0acd554edb8cf320b4cdb8731752b9d4e8ae1013
rshortt <rshortt@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-06-25 13:31:26 +0000

    testing wrapping GWK window with X11 one
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2729 a8f5125c-1e01-0410-8897-facf34644b8e

---
+++ display/test/gtk_imlib2.py
@@ -0,0 +1,24 @@
+import gtk
+
+import kaa
+import kaa.imlib2
+import kaa.display
+
+
+window = gtk.Window()
+window.set_size_request(1024, 768)
+da = gtk.DrawingArea()
+da.set_size_request(800, 600)
+    
+window.add(da)
+window.connect("destroy", gtk.main_quit)
+window.show_all()
+    
+image = kaa.imlib2.open("data/background.jpg")
+
+# print da.window.xid
+x11win = kaa.display.X11Window(window = da.window.xid)
+# x11win.render_imlib2_image(da.image)
+
+kaa.main()
+
