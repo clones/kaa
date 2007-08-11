9f3ed7ad714b016d14c71f708c955d00c7ea3132
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-08-11 18:59:03 +0000

    remove old beacon
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2770 a8f5125c-1e01-0410-8897-facf34644b8e

--- beacon/test/cdrom.py
+++
@@ -1,8 +0,0 @@
-import kaa
-import kaa.beacon
-
-kaa.beacon.connect()
-kaa.beacon.add_mountpoint('cdrom', '/dev/dvd', '/mnt/dvd')
-kaa.beacon.add_mountpoint('cdrom', '/dev/cdrom', '/mnt/cdrom')
-
-kaa.main()
--- beacon/test/show.py
+++
@@ -1,7 +0,0 @@
-import sys
-import kaa.beacon
-
-kaa.beacon.connect()
-file = kaa.beacon.get(sys.argv[1])
-for key in file.keys():
-	print key, file[key]
