7c4ebbe897b60f26db2e56047edd4ae79087e9af
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-08-31 18:34:55 +0000

    move generic Status object to kaa.notifier.Progress
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2790 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/netsearch/src/feed/download.py
+++ WIP/netsearch/src/feed/download.py
@@ -4,39 +4,6 @@ import urllib
 import urllib2
 import kaa.notifier
 
-class Status(kaa.notifier.Signal):
-    """
-    Generic status object for InProgress
-    """
-    def __init__(self):
-        super(Status,self).__init__()
-        self.percent = 0
-        self.pos = 0
-        self.max = 0
-
-    def set(self, pos, max=None):
-        if max is not None:
-            self.max = max
-        self.pos = pos
-        if pos > self.max:
-            self.max = pos
-        if self.max:
-            self.percent = (self.pos * 100) / self.max
-        else:
-            self.percent = 0
-        self.emit()
-
-    def update(self, diff):
-        self.set(self.pos + diff)
-
-
-    def __str__(self):
-        n = 0
-        if self.max:
-            n = int((self.pos / float(self.max)) * 50)
-        return "|%51s| %d / %d" % (("="*n + ">").ljust(51), self.pos, self.max)
-
-
 def fetch_HTTP(url, filename):
     """
     Fetch HTTP URL.
@@ -64,7 +31,7 @@ def fetch_HTTP(url, filename):
         # stupid url encoding in url
         url = url[:8+url[8:].find('/')] + \
               urllib.quote(url[8+url[8:].find('/'):])
-    s = Status()
+    s = kaa.notifier.Progress()
     t = kaa.notifier.Thread(download, url, filename, s)
     t.wait_on_exit(False)
     async = t.start()
