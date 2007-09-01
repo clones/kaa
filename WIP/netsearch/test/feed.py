d0be539388214d382cacef5f3bb2ff89f06c5817
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2007-09-01 20:23:49 +0000

    snapshot of test app, not working yet
    git-svn-id: svn://svn.freevo.org/kaa/trunk@2796 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/netsearch/test/feed.py
+++ WIP/netsearch/test/feed.py
@@ -1,44 +1,120 @@
 import sys
 import kaa.notifier
-from kaa.netsearch.feed import Channel
+import kaa.beacon
+import kaa.netsearch.feed
 
-# ##################################################################
-# test code
-# ##################################################################
+if len(sys.argv) > 1:
+    if sys.argv[1] in ('--update', '-u'):
+        if not kaa.netsearch.feed.list_channels():
+            print 'no channels defined'
+            sys.exit(0)
+        kaa.beacon.connect()
+        kaa.netsearch.feed.update().connect(sys.exit)
+        kaa.notifier.loop()
+        sys.exit(0)
 
-# class Filter(Channel):
+# import gtk for gui
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gtk.glade
 
-#     def __init__(self, channel, filter):
-#         Channel.__init__(self, None)
-#         self._channel = channel
-#         self._filter = filter
+kaa.notifier.init('gtk')
+# kaa.beacon.connect()
 
-#     def __iter__(self):
-#         for f in self._channel:
-#             if isinstance(f, kaa.notifier.InProgress):
-#                 # dummy entry to signal waiting
-#                 yield f
-#                 continue
-#             if self._filter(f):
-#                 yield f
+GLADEFILE = 'test/feedmanager.glade'
 
-@kaa.notifier.yield_execution()
-def update_feeds(*feeds):
-    for feed, destdir, num, download in feeds:
-        if download:
-            yield feed.update(destdir, num)
-        else:
-            yield feed.store_in_beacon(destdir, num)
+class Manager(object):
+    def __init__(self):
+        self.xml = gtk.glade.XML(GLADEFILE, 'manager')
+        self.xml.signal_autoconnect (self)
 
-kaa.beacon.connect()
-d = '/local/video/feedtest'
-update_feeds((Channel('http://podcast.wdr.de/blaubaer.xml'), d, 5, False),
-             (Channel('http://podcast.nationalgeographic.com/wild-chronicles/'), \
-              d, 5, False)).\
-             connect(sys.exit)
-#              (Channel('http://www.tagesschau.de/export/video-podcast'), d, 1, False),
-#              (YouTube(tags='robot chicken'), d, 2, True),
-#              (Stage6('stage6://Diva-Channel'), d, 5, False)).\
+        self.treestore = gtk.TreeStore(str)
+        self.tree = self.xml.get_widget("list")
+        self.tree.set_model(self.treestore)
+        column = gtk.TreeViewColumn("URL", gtk.CellRendererText(), text=0)
+        self.tree.append_column(column)
 
+        self.channels = []
+        for channel in kaa.netsearch.feed.list_channels():
+            self.treestore.append(None, [ channel.url ])
+            self.channels.append(channel)
+            
+    def on_add_clicked(self, args):
+        Edit()
+        
+    def on_configure_clicked(self, args):
+        sel = self.tree.get_selection().get_selected_rows()[1]
+        if not sel:
+            return
+        Edit(self.channels[sel[0][0]])
+    
+    def on_remove_clicked(self, args):
+        pass
+    
+    def on_quit_clicked(self, args):
+        sys.exit(0)
+
+
+class Edit(object):
+    def __init__(self, channel=None):
+        self.xml = gtk.glade.XML(GLADEFILE, 'edit')
+        self.xml.signal_autoconnect (self)
+        self.win = self.xml.get_widget("edit")
+        self.channel = channel
+        if channel:
+            for widget in ('url', 'dirname'):
+                self.get_widget(widget).set_text(getattr(channel, widget))
+                self.xml.get_widget(widget).set_sensitive(False)
+        self.win.show()
+
+
+    def get_widget(self, name):
+        return self.xml.get_widget(name)
+    
+
+    def on_all_items_toggled(self, button):
+        self.xml.get_widget("num").set_sensitive(not button.get_active())
+        self.xml.get_widget("num_label").set_sensitive(not button.get_active())
+        
+
+    def on_download_toggled(self, button):
+        self.xml.get_widget("keep").set_sensitive(button.get_active())
+
+
+    def on_cancel_clicked(self, args):
+        self.win.destroy()
+
+
+    def on_ok_clicked(self, args):
+        # get data
+        url = self.get_widget("url").get_text()
+        destdir = self.get_widget("dirname").get_text()
+        download = self.xml.get_widget("download").get_active()
+        keep = self.xml.get_widget("keep").get_active()
+        num = 0
+        if not self.xml.get_widget("all_items").get_active():
+            num = int(self.xml.get_widget("num").get_value())
+
+        if self.channel:
+            self.channel.configure(download, num, keep)
+            return
+
+        if url and destdir:
+            kaa.netsearch.feed.add_channel(url, destdir, download, num, keep)
+            return
+        
+        # no idea how this works with glade.
+        # FIXME: make the dialog modal
+        dialog = gtk.MessageDialog(
+            parent = self.win, flags = gtk.DIALOG_DESTROY_WITH_PARENT,
+            type = gtk.MESSAGE_INFO, buttons = gtk.BUTTONS_OK,
+            message_format = 'You need to fill in an URL and a directory where\n' + \
+            'the items should be stored.')
+        dialog.set_title('Error')
+        dialog.connect('response', lambda dialog, response: dialog.destroy())
+        dialog.show()
+        return
+    
+m = Manager()
 kaa.notifier.loop()
-
