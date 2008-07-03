984ec368b36afa5be3d6c4a7ae9620227cc8f9b6
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2008-07-03 10:48:10 +0000

    add support to load images from the web
    git-svn-id: svn://svn.freevo.org/kaa/trunk@3320 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/candy2/src/widgets/image.py
+++ WIP/candy2/src/widgets/image.py
@@ -29,15 +29,22 @@
 __all__ = [ 'Image', 'Thumbnail' ]
 
 # python imports
-import logging
 import os
+import md5
+import logging
 
 # clutter imports
 import gtk.gdk
 
+# kaa imports
+import kaa.net.url
+
 # kaa.candy imports imports
 from .. import config, threaded
 import core
+
+# get logging object
+log = logging.getLogger('kaa.candy')
 
 class Image(core.Texture):
     """
@@ -46,24 +53,55 @@ class Image(core.Texture):
     candyxml_name = 'image'
     context_sensitive = True
 
-    def __init__(self, pos, size, filename, context=None):
+    def __init__(self, pos, size, url, context=None):
         """
         @param pos: (x,y) position of the widget or None
         @param size: (width,height) geometry of the widget or None.
-        @param filename: filename of the image. If the image is not found
-            config.imagepath will be searched for the image. If the filename
-            starts with C{$} the filename will be searched in the context.
+        @param url: filename or url of the image. If the image is not found
+            config.imagepath will be searched for the image. If the url
+            starts with C{$} the url will be searched in the context.
         @param context: the context the widget is created in
-        @todo: add keep aspect
+        @todo: add keep aspect and add default image if not found or still fetched
         """
         super(Image, self).__init__(pos, size, context)
-        if filename and filename.startswith('$'):
-            self.set_dependency(filename[1:])
-            filename = eval(filename[1:], context)
-        if filename and not filename.startswith('/'):
-            filename = self._get_image(filename)
-        if filename:
-            self.set_pixbuf(gtk.gdk.pixbuf_new_from_file(filename))
+        if not url:
+            return
+        if url.startswith('$'):
+            # variable from the context, e.g. $varname
+            self.set_dependency(url[1:])
+            url = eval(url[1:], context)
+            if not url:
+                return
+        if url.startswith('http://'):
+            # remote image, create local cachefile
+            # FIXME: how to handle updates on the remote side?
+            base = md5.md5(url).hexdigest() + os.path.splitext(url)[1]
+            cachefile = kaa.tempfile('candy-images/' + base)
+            if not os.path.isfile(cachefile):
+                # Download the image
+                # FIXME: errors will be dropped
+                tmpfile = kaa.tempfile('candy-images/.' + base)
+                download = kaa.net.url.fetch(url, cachefile, tmpfile)
+                download.connect_weak_once(self._fetched, cachefile)
+                return
+            # use cachefile as image
+            url = cachefile
+        if not url.startswith('/'):
+            url = self._get_image(url)
+            if not url:
+                return
+        # load the image to the texture
+        self.set_pixbuf(gtk.gdk.pixbuf_new_from_file(url))
+
+    @threaded()
+    def _fetched(self, status, cachefile):
+        """
+        Callback for HTTP GET result. The image should be in the cachefile.
+        """
+        try:
+            self.set_pixbuf(gtk.gdk.pixbuf_new_from_file(cachefile))
+        except Exception, e:
+            log.exception('bad image: %s' % cachefile)
 
     def _get_image(self, name):
         """
@@ -79,10 +117,12 @@ class Image(core.Texture):
     @classmethod
     def candyxml_parse(cls, element):
         """
-        Parse the XML element for parameter to create the widget.
+        Parse the candyxml element for parameter to create the widget. Example::
+          <image x='10' y='10' width='200' height='100' filename='image.jpg'/>
+          <image width='200' height='100' url='http://www.exapmple.com/image.jpg'/>
         """
         return super(Image, cls).candyxml_parse(element).update(
-            filename=element.filename)
+            url=element.url or element.filename)
 
 
 class Thumbnail(Image):
@@ -90,7 +130,7 @@ class Thumbnail(Image):
     Widget showing a kaa.beacon.Thumbnail
     """
     candyxml_name = 'thumbnail'
-    
+
     def __init__(self, pos, size, thumbnail, context=None):
         """
         @param pos: (x,y) position of the widget or None
@@ -122,7 +162,7 @@ class Thumbnail(Image):
         if not self._thumbnail.is_failed():
             print self._thumbnail.get()
             self.set_pixbuf(gtk.gdk.pixbuf_new_from_file(self._thumbnail.get()))
-        
+
     @classmethod
     def candyxml_parse(cls, element):
         """
@@ -134,7 +174,7 @@ class Thumbnail(Image):
         return core.Texture.candyxml_parse(element).update(
             thumbnail=element.thumbnail)
 
-        
+
 # register widgets to candyxml
 Image.candyxml_register()
 Thumbnail.candyxml_register()
