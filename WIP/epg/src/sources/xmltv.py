90ea6ada331368d19cc63268c4d5a5ab64b101a0
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2008-09-16 16:48:52 +0000

    UTC fixes
    git-svn-id: svn://svn.freevo.org/kaa/trunk@3558 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/epg/src/sources/xmltv.py
+++ WIP/epg/src/sources/xmltv.py
@@ -181,13 +181,14 @@ class XmltvParser(object):
                 secs -= adj_secs
             else:
                 secs += adj_secs
-        else:
-            try:
-                secs = time.mktime(time.strptime(timestr,'%Y%m%d%H%M%S %Z'))
-            except ValueError:
-                #try without the timezone
-                secs = time.mktime(time.strptime(tval,'%Y%m%d%H%M%S'))
-        return float(secs)
+            return int(secs)
+        try:
+            secs = time.mktime(time.strptime(timestr,'%Y%m%d%H%M%S %Z'))
+        except ValueError:
+            #try without the timezone
+            secs = time.mktime(time.strptime(tval,'%Y%m%d%H%M%S'))
+        # adjust secs to UTC and return
+        return int(secs) - time.timezone
 
     def handle_channel(self, attr):
         """
