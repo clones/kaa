26298f28a91501e2f988e40805eaa50effbbe50d
tack <tack@a8f5125c-1e01-0410-8897-facf34644b8e>
2009-01-02 00:33:14 +0000

    New propery "reflect-bottom" (defaults to TRUE) which allows the reflection to be taken from either the top or bottom of the source texture.
    Fixes a bug in grid (e.g. flickr demo) in which reflections were completely
    screwed (reflection texture was placed on the bottom, but it was actually
    reflecting the top of the source texture).
    
    
    git-svn-id: svn://svn.freevo.org/kaa/trunk@3721 a8f5125c-1e01-0410-8897-facf34644b8e

--- candy/src/backend/libcandy/clutter-reflect-texture.c
+++ candy/src/backend/libcandy/clutter-reflect-texture.c
@@ -56,7 +56,8 @@
 enum
 {
   PROP_0,
-  PROP_REFLECTION_HEIGHT
+  PROP_REFLECTION_HEIGHT,
+  PROP_REFLECT_BOTTOM
 };
 
 G_DEFINE_TYPE (ClutterReflectTexture,
@@ -68,7 +69,7 @@ G_DEFINE_TYPE (ClutterReflectTexture,
 
 struct _ClutterReflectTexturePrivate
 {
-  gint                 reflection_height;
+  gint                 reflection_height, reflect_bottom;
 };
 
 
@@ -80,7 +81,9 @@ clutter_reflect_texture_paint (ClutterActor *actor)
   ClutterCloneTexture   *clone;
   ClutterTexture        *parent;
   guint                  width, height;
-  gint                   r_height;  
+  gint                   r_height;
+  gint                   opacity;
+  gint                   bottom;
 
   CoglHandle        cogl_texture;
   CoglTextureVertex tvert[4];
@@ -105,35 +108,37 @@ clutter_reflect_texture_paint (ClutterActor *actor)
   clutter_actor_get_size (actor, &width, &height);
 
   r_height = priv->reflection_height;
+  bottom = priv->reflect_bottom;
+  opacity = clutter_actor_get_opacity(actor);
 
   if (r_height < 0 || r_height > height)
     r_height = height;
 
 #define FX(x) CLUTTER_INT_TO_FIXED(x)
 
-  rty = CFX_DIV(FX(r_height),FX(height));
+  rty = CFX_DIV(FX(bottom ? height-r_height : r_height),FX(height));
 
   /* clockise vertices and tex coords and colors! */
 
   tvert[0].x = tvert[0].y = tvert[0].z = 0; 
-  tvert[0].tx = 0; tvert[0].ty = rty;  
+  tvert[0].tx = 0; tvert[0].ty = bottom ? CFX_ONE : rty;
   tvert[0].color.red = tvert[0].color.green = tvert[0].color.blue = 0xff;
-  tvert[0].color.alpha = clutter_actor_get_opacity (actor);
+  tvert[0].color.alpha = bottom ? opacity : 0;
 
   tvert[1].x = FX(width); tvert[1].y = tvert[1].z = 0; 
-  tvert[1].tx = CFX_ONE; tvert[1].ty = rty;  
+  tvert[1].tx = CFX_ONE; tvert[1].ty = bottom ? CFX_ONE : rty;  
   tvert[1].color.red = tvert[1].color.green = tvert[1].color.blue = 0xff;
-  tvert[1].color.alpha = clutter_actor_get_opacity (actor);
+  tvert[1].color.alpha = bottom ? opacity : 0;
 
   tvert[2].x = FX(width); tvert[2].y = FX(r_height); tvert[2].z = 0; 
-  tvert[2].tx = CFX_ONE; tvert[2].ty = 0;  
+  tvert[2].tx = CFX_ONE; tvert[2].ty = bottom ? rty : 0;  
   tvert[2].color.red = tvert[2].color.green = tvert[2].color.blue = 0xff;
-  tvert[2].color.alpha = 0;
+  tvert[2].color.alpha = bottom ? 0 : opacity;
 
   tvert[3].x = 0; tvert[3].y = FX(r_height); tvert[3].z = 0; 
-  tvert[3].tx = 0; tvert[3].ty = 0;  
+  tvert[3].tx = 0; tvert[3].ty = bottom ? rty : 0;  
   tvert[3].color.red = tvert[3].color.green = tvert[3].color.blue = 0xff;
-  tvert[3].color.alpha = 0;
+  tvert[3].color.alpha = bottom ? 0 : opacity;
 
   cogl_push_matrix ();
 
@@ -156,6 +161,9 @@ clutter_reflect_texture_set_property (GObject      *object,
     case PROP_REFLECTION_HEIGHT:
       priv->reflection_height = g_value_get_int (value);
       break;
+    case PROP_REFLECT_BOTTOM:
+      priv->reflect_bottom = g_value_get_boolean(value);
+      break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
       break;
@@ -175,6 +183,9 @@ clutter_reflect_texture_get_property (GObject    *object,
     {
     case PROP_REFLECTION_HEIGHT:
       g_value_set_int (value, priv->reflection_height);
+      break;
+    case PROP_REFLECT_BOTTOM:
+      g_value_set_boolean (value, priv->reflect_bottom);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
@@ -202,6 +213,14 @@ clutter_reflect_texture_class_init (ClutterReflectTextureClass *klass)
                                                      0,
                                                      (G_PARAM_CONSTRUCT | CLUTTER_PARAM_READWRITE)));
 
+  g_object_class_install_property (gobject_class,
+                                   PROP_REFLECT_BOTTOM,
+                                   g_param_spec_boolean("reflect-bottom",
+                                                       "Reflection on bottom (TRUE) or top (FALSE) of source",
+                                                       "",
+                                                       TRUE,
+                                                       (G_PARAM_CONSTRUCT | CLUTTER_PARAM_READWRITE)));
+
   g_type_class_add_private (gobject_class, sizeof (ClutterReflectTexturePrivate));
 }
 
@@ -212,6 +231,7 @@ clutter_reflect_texture_init (ClutterReflectTexture *self)
 
   self->priv = priv = CLUTTER_REFLECT_TEXTURE_GET_PRIVATE (self);
   priv->reflection_height = 100; 
+  priv->reflect_bottom = TRUE; 
 }
 
 /**
@@ -234,6 +254,7 @@ clutter_reflect_texture_new (ClutterTexture *texture, gint reflection_height)
   return g_object_new (CLUTTER_TYPE_REFLECT_TEXTURE,
  		       "parent-texture", texture,
 		       "reflection-height", reflection_height,
+		       "reflect-bottom", TRUE,
 		       NULL);
 }
 
