cf81294ad8771c7fd40b9c34ff13792e70f24f01
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2009-03-15 20:25:12 +0000

    handle all kinds of parser errors
    git-svn-id: svn://svn.freevo.org/kaa/trunk@3922 a8f5125c-1e01-0410-8897-facf34644b8e

--- beacon/src/server/parser.py
+++ beacon/src/server/parser.py
@@ -132,186 +132,191 @@ def _parse(db, item, mtime):
     """
     Parse the item, this can take a while.
     """
-
-    #
-    # Parent checking
-    #
-
-    parent = item._beacon_parent
-    if not parent._beacon_id:
-        # There is a parent without id, update the parent now.
-        r = parse(db, parent)
-        if isinstance(r, kaa.InProgress):
-            yield r
+    produced_load = 0
+    try:
+        #
+        # Parent checking
+        #
+    
+        parent = item._beacon_parent
         if not parent._beacon_id:
-            # This should never happen
-            raise AttributeError('parent for %s has no dbid' % item)
-        # we had no parent id which we have now. Restart the whole
-        # parsing process. maye this item was in the db already
-        r = parse(db, parent)
-        if isinstance(r, kaa.InProgress):
-            r = yield r
-        yield r
-
-
-    #
-    # Metadata parsing
-    #
-
-    t1 = time.time()
-
-    # FIXME: add force parameter from config file:
-    # - always force (slow but best result)
-    # - never force (faster but maybe wrong)
-    # - only force on media 1 (good default)
-
-    # Parse metadata in an extra named thread
-    metadata = yield parse_thread(item.filename)
-    if not metadata:
-        metadata = {}
-
-    attributes = { 'mtime': mtime, 'image': metadata.get('image') }
-
-    if metadata.get('media') == kaa.metadata.MEDIA_DISC and \
-           metadata.get('subtype') in db.list_object_types():
-        type = metadata['subtype']
-        if metadata.get('type'):
-            attributes['scheme'] = metadata.get('type').lower()
-        item._beacon_isdir = False
-    elif media_types.get(metadata.get('media')) in db.list_object_types():
-        type = media_types.get(metadata['media'])
-    elif item._beacon_isdir:
-        type = 'dir'
-    else:
-        type = 'file'
-
-    if item._beacon_id and type != item._beacon_id[0]:
-        # The item changed its type. Adjust the db
+            # There is a parent without id, update the parent now.
+            r = parse(db, parent)
+            if isinstance(r, kaa.InProgress):
+                yield r
+            if not parent._beacon_id:
+                # This should never happen
+                raise AttributeError('parent for %s has no dbid' % item)
+            # we had no parent id which we have now. Restart the whole
+            # parsing process. maye this item was in the db already
+            r = parse(db, parent)
+            if isinstance(r, kaa.InProgress):
+                r = yield r
+            yield r
+    
+    
+        #
+        # Metadata parsing
+        #
+    
+        t1 = time.time()
+    
+        # FIXME: add force parameter from config file:
+        # - always force (slow but best result)
+        # - never force (faster but maybe wrong)
+        # - only force on media 1 (good default)
+    
+        # Parse metadata in an extra named thread
+        metadata = yield parse_thread(item.filename)
+        if not metadata:
+            metadata = {}
+    
+        attributes = { 'mtime': mtime, 'image': metadata.get('image') }
+    
+        if metadata.get('media') == kaa.metadata.MEDIA_DISC and \
+               metadata.get('subtype') in db.list_object_types():
+            type = metadata['subtype']
+            if metadata.get('type'):
+                attributes['scheme'] = metadata.get('type').lower()
+            item._beacon_isdir = False
+        elif media_types.get(metadata.get('media')) in db.list_object_types():
+            type = media_types.get(metadata['media'])
+        elif item._beacon_isdir:
+            type = 'dir'
+        else:
+            type = 'file'
+    
+        if item._beacon_id and type != item._beacon_id[0]:
+            # The item changed its type. Adjust the db
+            while db.read_lock.is_locked():
+                # wait for the db to be free for write access
+                yield db.read_lock.yield_unlock()
+            data = db.update_object_type(item._beacon_id, type)
+            if not data:
+                log.error('item to change not in the db anymore')
+            log.info('change item %s to %s' % (item._beacon_id, type))
+            item._beacon_database_update(data)
+    
+    
+        #
+        # Thumbnail / Cover / Image stuff.
+        #
+    
+        produced_load = 1
+    
+        if type == 'dir':
+            attributes['image_from_items'] = False
+            if not attributes.get('image'):
+                for cover in ('cover.jpg', 'cover.png'):
+                    if os.path.isfile(item.filename + cover):
+                        attributes['image'] = item.filename + cover
+                        break
+    
+            # TODO: do some more stuff here:
+            # Audio directories may have a different cover if there is only
+            # one jpg in a dir of mp3 files or a files with 'front' in the name.
+            # They need to be added here as special kind of cover
+    
+        elif type == 'image':
+            attributes['image'] = item.filename
+            if metadata.get('thumbnail'):
+                t = thumbnail.Thumbnail(item.filename, item._beacon_media)
+                if t.needs_update:
+                    # only store the normal version
+                    try:
+                        produced_load = 2
+                        t.normal = kaa.imlib2.open_from_memory(metadata.get('thumbnail'))
+                    except (ValueError, IOError):
+                        log.exception('image thumbnail')
+        else:
+            base = os.path.splitext(item.filename)[0]
+            if type == 'video' and not attributes.get('image') and \
+                   thumbnail.SUPPORT_VIDEO:
+                attributes['image'] = item.filename
+            if metadata.get('thumbnail') and not attributes.get('image'):
+                attributes['image'] = item.filename
+                t = thumbnail.Thumbnail(item.filename, item._beacon_media)
+                try:
+                    produced_load = 2
+                    t.image = kaa.imlib2.open_from_memory(metadata['thumbnail'])
+                except (ValueError, IOError):
+                    log.exception('raw thumbnail')
+            for ext in ('.jpg', '.png'):
+                if os.path.isfile(base + ext):
+                    attributes['image'] = base + ext
+                    break
+                if os.path.isfile(item.filename + ext):
+                    attributes['image'] = item.filename + ext
+                    break
+    
+        attributes['metadata'] = metadata
+    
+        # now call extention plugins
+        ext = os.path.splitext(item.filename)[1]
+        for function in extention_plugins.get(ext, []) + extention_plugins.get(None, []):
+            function(item, attributes, type)
         while db.read_lock.is_locked():
             # wait for the db to be free for write access
             yield db.read_lock.yield_unlock()
-        data = db.update_object_type(item._beacon_id, type)
-        if not data:
-            log.error('item to change not in the db anymore')
-        log.info('change item %s to %s' % (item._beacon_id, type))
-        item._beacon_database_update(data)
+    
+        if attributes.get('image'):
+            # create thumbnail
+            t = thumbnail.Thumbnail(attributes.get('image'), item._beacon_media)
+            if t.needs_update and (not type == 'video' or not hasattr(item, 'filename') or
+                    utils.do_thumbnail(item.filename)):
+                t.create(t.PRIORITY_LOW)
+    
+        if not metadata.get('title'):
+            # try to set a good title
+            # FIXME: if a file is renamed later, this tite should also update
+            title = get_title(item._beacon_data['name'], strip=not item._beacon_isdir)
+            metadata['title'] = kaa.str_to_unicode(title)
+    
+    
+        #
+        # Database code
+        #
+        # add kaa.metadata results, the db module will add everything known
+        # to the db. After that add or update the database.
+        #
+    
+        if item._beacon_id:
+            # Update old db entry
+            db.update_object(item._beacon_id, **attributes)
+            item._beacon_data.update(attributes)
+        else:
+            # Create new entry
+            obj = db.add_object(type, name=item._beacon_data['name'], parent=parent, overlay=item._beacon_overlay, **attributes)
+            item._beacon_database_update(obj)
+    
+        #
+        # Additional track handling
+        #
+    
+        if hasattr(metadata, 'tracks'):
+            # The item has tracks, e.g. a dvd image on hd.
+            if not metadata.get('type'):
+                log.error('%s metadata has no type', item)
+                yield produced_load
+    
+            # delete all known tracks before adding new
+            result = yield db.query(parent=item)
+            for track in result:
+                db.delete_object(track)
+    
+            if not 'track_%s' % metadata.get('type').lower() in \
+                   db.list_object_types():
+                key = metadata.get('type').lower()
+                log.error('track_%s not in database keys', key)
+                yield produced_load
+            type = 'track_%s' % metadata.get('type').lower()
+            for track in metadata.tracks:
+                db.add_object(type, name=str(track.trackno), parent=item, metadata=track)
+    
+        # parsing done
+        log.info('scan %s (%0.3f)' % (item, time.time() - t1))
 
+    except Exception, e:
+        log.exception('parser error: %s', item)
 
-    #
-    # Thumbnail / Cover / Image stuff.
-    #
-
-    produced_load = 1
-
-    if type == 'dir':
-        attributes['image_from_items'] = False
-        if not attributes.get('image'):
-            for cover in ('cover.jpg', 'cover.png'):
-                if os.path.isfile(item.filename + cover):
-                    attributes['image'] = item.filename + cover
-                    break
-
-        # TODO: do some more stuff here:
-        # Audio directories may have a different cover if there is only
-        # one jpg in a dir of mp3 files or a files with 'front' in the name.
-        # They need to be added here as special kind of cover
-
-    elif type == 'image':
-        attributes['image'] = item.filename
-        if metadata.get('thumbnail'):
-            t = thumbnail.Thumbnail(item.filename, item._beacon_media)
-            if t.needs_update:
-                # only store the normal version
-                try:
-                    produced_load = 2
-                    t.normal = kaa.imlib2.open_from_memory(metadata.get('thumbnail'))
-                except (ValueError, IOError):
-                    log.exception('image thumbnail')
-    else:
-        base = os.path.splitext(item.filename)[0]
-        if type == 'video' and not attributes.get('image') and \
-               thumbnail.SUPPORT_VIDEO:
-            attributes['image'] = item.filename
-        if metadata.get('thumbnail') and not attributes.get('image'):
-            attributes['image'] = item.filename
-            t = thumbnail.Thumbnail(item.filename, item._beacon_media)
-            try:
-                produced_load = 2
-                t.image = kaa.imlib2.open_from_memory(metadata['thumbnail'])
-            except (ValueError, IOError):
-                log.exception('raw thumbnail')
-        for ext in ('.jpg', '.png'):
-            if os.path.isfile(base + ext):
-                attributes['image'] = base + ext
-                break
-            if os.path.isfile(item.filename + ext):
-                attributes['image'] = item.filename + ext
-                break
-
-    attributes['metadata'] = metadata
-
-    # now call extention plugins
-    ext = os.path.splitext(item.filename)[1]
-    for function in extention_plugins.get(ext, []) + extention_plugins.get(None, []):
-        function(item, attributes, type)
-    while db.read_lock.is_locked():
-        # wait for the db to be free for write access
-        yield db.read_lock.yield_unlock()
-
-    if attributes.get('image'):
-        # create thumbnail
-        t = thumbnail.Thumbnail(attributes.get('image'), item._beacon_media)
-        if t.needs_update and (not type == 'video' or not hasattr(item, 'filename') or
-                utils.do_thumbnail(item.filename)):
-            t.create(t.PRIORITY_LOW)
-
-    if not metadata.get('title'):
-        # try to set a good title
-        # FIXME: if a file is renamed later, this tite should also update
-        title = get_title(item._beacon_data['name'], strip=not item._beacon_isdir)
-        metadata['title'] = kaa.str_to_unicode(title)
-
-
-    #
-    # Database code
-    #
-    # add kaa.metadata results, the db module will add everything known
-    # to the db. After that add or update the database.
-    #
-
-    if item._beacon_id:
-        # Update old db entry
-        db.update_object(item._beacon_id, **attributes)
-        item._beacon_data.update(attributes)
-    else:
-        # Create new entry
-        obj = db.add_object(type, name=item._beacon_data['name'], parent=parent, overlay=item._beacon_overlay, **attributes)
-        item._beacon_database_update(obj)
-
-    #
-    # Additional track handling
-    #
-
-    if hasattr(metadata, 'tracks'):
-        # The item has tracks, e.g. a dvd image on hd.
-        if not metadata.get('type'):
-            log.error('%s metadata has no type', item)
-            yield produced_load
-
-        # delete all known tracks before adding new
-        result = yield db.query(parent=item)
-        for track in result:
-            db.delete_object(track)
-
-        if not 'track_%s' % metadata.get('type').lower() in \
-               db.list_object_types():
-            key = metadata.get('type').lower()
-            log.error('track_%s not in database keys', key)
-            yield produced_load
-        type = 'track_%s' % metadata.get('type').lower()
-        for track in metadata.tracks:
-            db.add_object(type, name=str(track.trackno), parent=item, metadata=track)
-
-    # parsing done
-    log.info('scan %s (%0.3f)' % (item, time.time() - t1))
     yield produced_load
