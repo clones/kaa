755de45a4843c5a7c6746632997cca115677d5b4
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2009-04-22 18:24:54 +0000

    remove old code
    git-svn-id: svn://svn.freevo.org/kaa/trunk@3976 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/netsearch/src/feed/plugins/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for plugin in os.listdir(os.path.dirname(__file__)):
-    if plugin.endswith('.py') and not plugin == '__init__.py':
-        exec('import %s' % os.path.splitext(plugin)[0])
-    
