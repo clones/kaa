6e7269ca4e14ceb94a38ddc16e3f7e717c243291
dmeyer <dmeyer@a8f5125c-1e01-0410-8897-facf34644b8e>
2009-05-15 18:13:49 +0000

    try to be smart
    git-svn-id: svn://svn.freevo.org/kaa/trunk@3985 a8f5125c-1e01-0410-8897-facf34644b8e

--- WIP/webmetadata/src/__init__.py
+++ WIP/webmetadata/src/__init__.py
@@ -8,8 +8,13 @@ def searchstring(filename):
     for part in re.split('[\._ -]', os.path.splitext(os.path.basename(filename))[0]):
         if part.lower() in REMOVE_FROM_SEARCH:
             break
+        try:
+            if len(search) and int(part) > 1900 and int(part) < 2100:
+                return search.strip(), int(part)
+        except ValueError:
+            pass
         search += ' ' + part
-    return search
+    return search.strip(), None
 
 def movie(search):
     return imdb.search(search)
