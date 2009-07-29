clones/kaa

commit c65a1efde243fdfdd053d12de658c61b8148334d

kaa.db bug fixes:  - when updating an object by clearing an ATTR_SIMPLE attribute, the pickle was    being updated in the database.  - inverted index searches with multiple terms where where one of the terms    had more than 200 matches could miss results.  - fetching an unset attribute with flags ATTR_SIMPLE | ATTR_INVERTED_INDEX    would return an empty list instead of None -- it should only do that when    the attribute is named after an inverted index, e.g.:       'tags': (list, ATTR_SIMPLE | ATTR_INVERTED_INDEX, 'tags')
git-svn-id: svn://svn.freevo.org/kaa/trunk@4213 a8f5125c-1e01-0410-8897-facf34644b8e

base/src/db.py
@@ -723,15 +723,18 @@ class Database:
     def _make_query_from_attrs(self, query_type, attrs, type_name):
         type_attrs = self._get_type_attrs(type_name)
 
+        # True if an attribute from the pickle was removed and we must force an update.
+        pickle_attr_removed = False
         columns = []
         values = []
         placeholders = []
 
         for key in attrs.keys():
-            if attrs[key] == None:
-                del attrs[key]
             if key not in type_attrs:
                 raise ValueError, "Reference to undefined attribute '%s' for type '%s'" % (key, type_name)
+            if attrs[key] == None and type_attrs[key][1] & ATTR_SIMPLE:
+                pickle_attr_removed = True
+                del attrs[key]
 
         attrs_copy = attrs.copy()
         for name, (attr_type, flags, attr_ivtidx, attr_split) in type_attrs.items():
@@ -761,7 +764,7 @@ class Database:
                 values.append(value)
                 del attrs_copy[name]
 
-        if len(attrs_copy) > 0:
+        if len(attrs_copy) > 0 or (pickle_attr_removed and query_type == 'update'):
             # From the remaining attributes, remove those named after inverted
             # indexes that aren't explicitly registered as attributes in the
             # object type.  Here is where we keep our cached copy of inverted
@@ -1116,7 +1119,7 @@ class Database:
                 # terms in object.
                 if not terms and ivtidx in orig_attrs:
                     # Update removed all terms for this ivtidx, remove from pickle.
-                    del orig_attrs[ivtidx]
+                    orig_attrs[ivtidx] = None
                 elif terms:
                     # There are terms for this ivtidx, store in pickle.
                     orig_attrs[ivtidx] = terms.keys()
@@ -1124,6 +1127,7 @@ class Database:
         query, values = self._make_query_from_attrs("update", orig_attrs, object_type)
         self._db_query(query, values)
         self._set_dirty()
+        # TODO: if an objectrow was given, return an updated objectrow
 
 
     def commit(self):
@@ -1655,10 +1659,10 @@ class Database:
 
                     if object_type == None:
                         q %= ''
-                        v = (id, rank, sql_limit, state[id]["offset"][rank])
+                        v = [id, rank, sql_limit, state[id]["offset"][rank]]
                     else:
                         q %= 'AND object_type=?'
-                        v = (id, rank, object_type, sql_limit, state[id]["offset"][rank])
+                        v = [id, rank, object_type, sql_limit, state[id]["offset"][rank]]
 
                     if id_constraints:
                         # We know about all objects that match one or more of the other
@@ -1668,6 +1672,10 @@ class Database:
                         # XXX: This can't benefit from the index if object_type
                         # is not specified.
                         q %= ' AND object_id IN %s' % _list_to_printable(tuple(id_constraints))
+                        # But since we're specifying a list of ids to search for with this
+                        # term, we can't use limit/offset, since the constraints might be
+                        # different since the last iteration.
+                        v[-2:] = [-1, 0]
                     else:
                         q %= ''
 
@@ -1686,7 +1694,7 @@ class Database:
                         # all the results we just got now intersect with our
                         # constraints set, we're done this term and don't bother
                         # querying it at other ranks.
-                        #print 'Done term '%s' at rank %d' % (terms[id]['term'], rank)
+                        #print "Done term '%s' at rank %d" % (terms[id]['term'], rank)
                         state[id]['done'] = True
                         if id_constraints is not None:
                             id_constraints = id_constraints.intersection(terms[id]['ids'])

base/src/extensions/objectrow.c
@@ -62,7 +62,8 @@ typedef struct {
 typedef struct {
     int index,       // Index in the sql row for this attribute, or -1 if none
         pickled,     // If we need to look in the pickle for this attribute.
-        flags;       // Attribute flags from kaa.Database definition.
+        flags,       // Attribute flags from kaa.Database definition.
+        named_ivtidx;// Attribute has same name as inverted index
     PyObject *type;  // Type object for this attribute
 } ObjectAttribute;
 
@@ -191,6 +192,7 @@ int ObjectRow_PyObject__init(ObjectRow_PyObject *self, PyObject *args, PyObject 
             }
             attr->type = PySequence_Fast_GET_ITEM(value, 0);
             attr->flags = PyInt_AsLong(PySequence_Fast_GET_ITEM(value, 1));
+            attr->named_ivtidx = PyObject_Compare(PySequence_Fast_GET_ITEM(value, 2), key) == 0;
             if (IS_ATTR_INDEXED_IGNORE_CASE(attr->flags) || attr->flags & ATTR_SIMPLE)
                 // attribute is set to ignore case, or it's ATTR_SIMPLE, so we
                 // need to look in the pickle for this attribute.
@@ -298,8 +300,8 @@ convert(ObjectRow_PyObject *self, ObjectAttribute *attr, PyObject *value)
 static PyObject *
 get_default_for_attr(ObjectAttribute *attr)
 {
-    if (attr->flags & ATTR_INVERTED_INDEX)
-        // If the attr is an inverted index, return an empty list.
+    if (attr->named_ivtidx)
+        // If the attr named after an inverted index, return an empty list.
         return PyList_New(0);
 
     // Otherwise return None.

commit 0dfeaf71d0c7b2cfe4109947d8e9f87c786fb4f5

Fix regression introduced in r4213
git-svn-id: svn://svn.freevo.org/kaa/trunk@4214 a8f5125c-1e01-0410-8897-facf34644b8e

base/src/db.py
@@ -731,9 +731,14 @@ class Database:
 
         for key in attrs.keys():
             if key not in type_attrs:
-                raise ValueError, "Reference to undefined attribute '%s' for type '%s'" % (key, type_name)
-            if attrs[key] == None and type_attrs[key][1] & ATTR_SIMPLE:
-                pickle_attr_removed = True
+                raise ValueError("Reference to undefined attribute '%s' for type '%s'" % (key, type_name))
+            if attrs[key] == None:
+                # Remove all None attributes (even ATTR_SEARCHABLE), otherwise we will
+                # raise a TypeError later, since NoneType isn't the right type.
+                if type_attrs[key][1] & ATTR_SIMPLE:
+                    # Attribute removed from a pickle, be sure we update the pickle in
+                    # the db if this is an 'update' query_type.
+                    pickle_attr_removed = True
                 del attrs[key]
 
         attrs_copy = attrs.copy()
@@ -756,8 +761,8 @@ class Database:
                     value = value.lower()
 
                 if attr_type != type(value):
-                    raise ValueError, "Type mismatch in query for %s: '%s' (%s) is not a %s" % \
-                                          (name, str(value), str(type(value)), str(attr_type))
+                    raise TypeError("Type mismatch in query for %s: '%s' (%s) is not a %s" % \
+                                    (name, str(value), str(type(value)), str(attr_type)))
                 if attr_type == str:
                     # Treat strings (non-unicode) as buffers.
                     value = buffer(value)