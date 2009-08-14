clones/kaa

commit 80ef8dc1a84a2488dd0f09f61c1b41957b1b1a8e

Catch exceptions that might have been raised when writing to socket and close rpc channel if that happens.  Should fix spurious unhandled exceptions with beacon-search.
git-svn-id: svn://svn.freevo.org/kaa/trunk@4026 a8f5125c-1e01-0410-8897-facf34644b8e

base/src/rpc.py
@@ -328,10 +328,24 @@ class Channel(Object):
         return self._connect_inprogress
 
 
+    def _write(self, data):
+        """
+        Writes data to the channel.
+        """
+        cb = kaa.WeakCallback(self._handle_close, False)
+        cb.ignore_caller_args = True
+        self._socket.write(data).exception.connect(cb)
+
+
     def _handle_close(self, expected, reset_signals=True):
         """
         kaa.Socket callback invoked when socket is closed.
         """
+        if not self._socket or not self._socket.alive:
+            # Socket already closed.  Return False to indicate the exception
+            # was handled, if invoked from write() exception handler.
+            return False
+
         if not self._authenticated:
             # Socket closed before authentication completed.  We assume it's
             # because authentication failed (though it may not be).
@@ -350,6 +364,9 @@ class Channel(Object):
                 # Raise an error if this happens during runtime or if
                 # someone wants to get the result or exception.
                 callback.throw(IOError, IOError('kaa.rpc channel closed'), None)
+
+        # Return False for reason explained above.
+        return False
 
 
     def _handle_read(self, data):
@@ -429,7 +446,7 @@ class Channel(Object):
             log.debug('delay packet %s', packet_type)
             self._write_buffer_deferred.append(header + payload)
         else:
-            self._socket.write(header + payload)
+            self._write(header + payload)
 
 
     def _send_answer(self, answer, seq):
@@ -669,7 +686,7 @@ class Channel(Object):
                 log.debug('Sent response to challenge from client.')
 
             # Empty deferred write buffer now that we're authenticated.
-            self._socket.write(''.join(self._write_buffer_deferred))
+            self._write(''.join(self._write_buffer_deferred))
             self._write_buffer_deferred = []
             self._handle_connected()
 

commit 680c151e1b430bbc0e413682fb251f35deca7d23

Fix regression introduced in r4026 in which the 'closed' signal would never be emitted.
git-svn-id: svn://svn.freevo.org/kaa/trunk@4218 a8f5125c-1e01-0410-8897-facf34644b8e

base/src/rpc.py
@@ -328,18 +328,18 @@ class Channel(Object):
         """
         Writes data to the channel.
         """
-        cb = kaa.WeakCallable(self._handle_close, False)
+        cb = self._socket.write(data).exception.connect_weak(self._handle_close, False, write_failed=True)
         cb.ignore_caller_args = True
-        self._socket.write(data).exception.connect(cb)
 
 
-    def _handle_close(self, expected, reset_signals=True):
+    def _handle_close(self, expected, reset_signals=True, write_failed=False):
         """
         kaa.Socket callback invoked when socket is closed.
         """
-        if not self._socket or not self._socket.alive:
+        if not self._socket or (write_failed and not self._socket.alive):
             # Socket already closed.  Return False to indicate the exception
-            # was handled, if invoked from write() exception handler.
+            # was handled, if invoked from write() exception handler (see
+            # Channel._write())
             return False
 
         if not self._authenticated: