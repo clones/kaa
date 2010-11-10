clones/kaa

commit 574431ddb691c2e9a72198e6b56bd19eb8aafafe

Font.get_text_size(): include size incurred by text styling.
git-svn-id: svn://svn.freevo.org/kaa/trunk@4379 a8f5125c-1e01-0410-8897-facf34644b8e

imlib2/src/font.py
@@ -254,7 +254,10 @@ class Font(object):
         :type text: str or unicode
         :returns: 4-tuple (width, height, horizontal advance, vertical advance)
         """
-        return self._font.get_text_size(utf8(text))
+        w, h, h_adv, v_adv = self._font.get_text_size(utf8(text))
+        # Include size of any styling.
+        style_w, style_h = self.get_style_geometry()[-2:]
+        return w + style_w, h + style_h, h_adv + style_w + v_adv + style_h
 
 
     def set_color(self, color):

commit 0b0b544f578c03c3c400ecb4c38537ac5102af9e

Fix regression in Font.get_text_size() introduced in r4379.  Fix regression in Image.blend() introduced in r4381.
git-svn-id: svn://svn.freevo.org/kaa/trunk@4415 a8f5125c-1e01-0410-8897-facf34644b8e

imlib2/src/font.py
@@ -267,7 +267,7 @@ class Font(object):
         w, h, h_adv, v_adv = self._font.get_text_size(utf8(text))
         # Include size of any styling.
         style_w, style_h = self.get_style_geometry()[-2:]
-        return w + style_w, h + style_h, h_adv + style_w + v_adv + style_h
+        return w + style_w, h + style_h, h_adv + style_w, v_adv + style_h
 
 
     def set_color(self, color):

imlib2/src/image.py
@@ -841,7 +841,7 @@ class Image(Object):
         if dst_size[1] == -1:
             dst_size = dst_size[0], src_size[1]
 
-        self._image.blend(src._image, src_pos, src_size, (x, y), dst_size, int(alpha), merge_alpha)
+        self._image.blend(src._image, src_pos, src_size, dst_pos, dst_size, int(alpha), merge_alpha)
         self._changed()
         return self
 